4e7dac27736a1f7c9b2ad7727777ab2acc4e95d4
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-09-25 05:47:24 +0000

    initial import of plugin from private repository (ignoring fully-generated files)

---
+++ trunk/org.openiaml.model.diagram.visual/src/org/openiaml/model/model/diagram/visual/view/factories/SyncWireViewFactory.java
@@ -0,0 +1,63 @@
+package org.openiaml.model.model.diagram.visual.view.factories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
+import org.eclipse.gmf.runtime.diagram.ui.view.factories.ConnectionViewFactory;
+import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
+import org.eclipse.gmf.runtime.notation.HintedDiagramLinkStyle;
+import org.eclipse.gmf.runtime.notation.NotationFactory;
+import org.eclipse.gmf.runtime.notation.View;
+import org.openiaml.model.model.diagram.visual.edit.parts.SyncWireEditPart;
+import org.openiaml.model.model.diagram.visual.edit.parts.SyncWireNameEditPart;
+import org.openiaml.model.model.diagram.visual.part.IamlVisualIDRegistry;
+
+/**
+ * @generated
+ */
+public class SyncWireViewFactory extends ConnectionViewFactory {
+
+	/**
+	 * @generated NOT
+	 */
+	protected List createStyles(View view) {
+		List styles = new ArrayList();
+		styles.add(NotationFactory.eINSTANCE.createConnectorStyle());
+		styles.add(NotationFactory.eINSTANCE.createFontStyle());
+
+		// add link to wire diagram (added manually)
+		{
+			HintedDiagramLinkStyle diagramFacet = NotationFactory.eINSTANCE
+					.createHintedDiagramLinkStyle();
+			diagramFacet.setHint("Iaml_Wire"); // $NON-NLS-1$
+			styles.add(diagramFacet);
+		}
+		return styles;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void decorateView(View containerView, View view,
+			IAdaptable semanticAdapter, String semanticHint, int index,
+			boolean persisted) {
+		if (semanticHint == null) {
+			semanticHint = IamlVisualIDRegistry
+					.getType(SyncWireEditPart.VISUAL_ID);
+			view.setType(semanticHint);
+		}
+		super.decorateView(containerView, view, semanticAdapter, semanticHint,
+				index, persisted);
+		IAdaptable eObjectAdapter = null;
+		EObject eObject = (EObject) semanticAdapter.getAdapter(EObject.class);
+		if (eObject != null) {
+			eObjectAdapter = new EObjectAdapter(eObject);
+		}
+		getViewService().createNode(eObjectAdapter, view,
+				IamlVisualIDRegistry.getType(SyncWireNameEditPart.VISUAL_ID),
+				ViewUtil.APPEND, true, getPreferencesHint());
+	}
+}

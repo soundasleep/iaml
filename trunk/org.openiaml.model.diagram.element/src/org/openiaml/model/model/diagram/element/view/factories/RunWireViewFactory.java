6dfc0ba79e971d124ad882c6a3a8bdd6ee81e541
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-09-29 00:14:56 +0000

    creating a new diagram editor for ApplicationElements (ignoring fully-generated files)

---
+++ trunk/org.openiaml.model.diagram.element/src/org/openiaml/model/model/diagram/element/view/factories/RunWireViewFactory.java
@@ -0,0 +1,54 @@
+package org.openiaml.model.model.diagram.element.view.factories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
+import org.eclipse.gmf.runtime.diagram.ui.view.factories.ConnectionViewFactory;
+import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
+import org.eclipse.gmf.runtime.notation.NotationFactory;
+import org.eclipse.gmf.runtime.notation.View;
+import org.openiaml.model.model.diagram.element.edit.parts.RunWireEditPart;
+import org.openiaml.model.model.diagram.element.edit.parts.RunWireNameEditPart;
+import org.openiaml.model.model.diagram.element.part.IamlVisualIDRegistry;
+
+/**
+ * @generated
+ */
+public class RunWireViewFactory extends ConnectionViewFactory {
+
+	/**
+	 * @generated
+	 */
+	protected List createStyles(View view) {
+		List styles = new ArrayList();
+		styles.add(NotationFactory.eINSTANCE.createConnectorStyle());
+		styles.add(NotationFactory.eINSTANCE.createFontStyle());
+		return styles;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void decorateView(View containerView, View view,
+			IAdaptable semanticAdapter, String semanticHint, int index,
+			boolean persisted) {
+		if (semanticHint == null) {
+			semanticHint = IamlVisualIDRegistry
+					.getType(RunWireEditPart.VISUAL_ID);
+			view.setType(semanticHint);
+		}
+		super.decorateView(containerView, view, semanticAdapter, semanticHint,
+				index, persisted);
+		IAdaptable eObjectAdapter = null;
+		EObject eObject = (EObject) semanticAdapter.getAdapter(EObject.class);
+		if (eObject != null) {
+			eObjectAdapter = new EObjectAdapter(eObject);
+		}
+		getViewService().createNode(eObjectAdapter, view,
+				IamlVisualIDRegistry.getType(RunWireNameEditPart.VISUAL_ID),
+				ViewUtil.APPEND, true, getPreferencesHint());
+	}
+}

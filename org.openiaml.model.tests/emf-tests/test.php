6b6cc0389eea970a1f53ae849eb3e41b47455123
Jevon Wright <soundasleep@users.noreply.github.com>
2008-10-07 09:11:07 +0000

    completing a test case... considering moving to PHPunit

--- org.openiaml.model.tests/emf-tests/test.php
+++ org.openiaml.model.tests/emf-tests/test.php
@@ -1,20 +1,14 @@
 <?php
 
+define("GMF_ROOT", "../../org.openiaml.model/model/");
+
 // get all the test cases that follow a certain file name
 
-$testCases = array();
-if ($handle = opendir('.')) {
-    while (false !== ($file = readdir($handle))) {
-        if ($file != "." && $file != ".." && substr(strtolower($file), -4) == ".php" && substr(strtolower($file), 0, 5) == "case.") {
-            $className = substr(substr($file, 0, strpos($file, ".php")), 5);
-            $testCases[] = $className;
-        }
-    }
-    closedir($handle);
-}
+$testCases = FileFinder::instance()->match("./", ".php", "case.");
+print_r($testCases);
 
 // load each test case
-foreach ($testCases as $test) {
+foreach ($testCases as $test => $testFile) {
 	try {
 		require("case.$test.php");
 		$class = new $test();
@@ -30,7 +24,9 @@ foreach ($testCases as $test) {
 function fatal($msg) {
 	echo "ERR: $msg\n";
 }
-
+function warn($msg) {
+	echo "! $msg\n";
+}
 
 $_singleton_instances = array();
 class XmlLoader {
@@ -53,8 +49,63 @@ class XmlLoader {
 	}
 }
 
+// TODO: move to PHPUnit?
+
 class GmfTestCase {
 	public function run() {
-		die("no run() specified in " . get_class($this));
+		fatal("no run() specified in " . get_class($this));
+	}
+
+	protected function toArray($xml) {
+		$r = array();
+
+		if (is_array($xml)) {
+			foreach ($xml as $k => $v) {
+				$r["$k"] = "$v";
+			}
+		}
+
+		return $r;
+	}
+
+	protected function assert($condition, $message = "expected(true), got(false)") {
+		if (!$condition) {
+			warn($message);
+		}
+	}
+
+	protected function assertInArray($needle, $haystack, $message = "") {
+		if (!$message) {
+			$message = "expected '$needle' in '$haystack'";
+		}
+		$this->assert(in_array($needle, $haystack), $message);
+	}
+}
+
+// find all files in a directory that match a certain pattern
+class FileFinder {
+	var $_loaded = array();
+
+	public function match($dir, $extension, $prefix = "") {
+		$result = array();
+		if ($handle = opendir($dir)) {
+			while (false !== ($file = readdir($handle))) {
+				if ($file != "." && $file != ".." && substr(strtolower($file), -strlen($extension)) == $extension && substr(strtolower($file), 0, strlen($prefix)) == $prefix) {
+					$className = substr(substr($file, 0, strpos($file, $extension)), strlen($prefix));
+					$result[$className] = $dir . $file;
+				}
+			}
+			closedir($handle);
+		}
+
+		return $result;
+	}
+
+	static public function instance() {
+		global $_singleton_instances;
+		if (!isset($_singleton_instances["FileFinder"])) {
+			$_singleton_instances["FileFinder"] = new FileFinder();
+		}
+		return $_singleton_instances["FileFinder"];
 	}
 }

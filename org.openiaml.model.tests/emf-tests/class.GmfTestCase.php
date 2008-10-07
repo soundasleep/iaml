cff2699ec5f7c9c13b41a2cff85c714dc64777b6
Jevon Wright <soundasleep@users.noreply.github.com>
2008-10-07 09:27:02 +0000

    cleaning up the test suite

---
+++ org.openiaml.model.tests/emf-tests/class.GmfTestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+class GmfTestCase {
+  var $runner;
+  public function setup($runner) {
+    $this->runner = $runner;
+  }
+  protected function fail($message) {
+    $this->runner->fail($message);
+  }
+  protected function pass($message) {
+    $this->runner->pass($message);
+  }
+
+  // for subclasses to override
+  public function run() {
+    fatal("no run() specified in " . get_class($this));
+  }
+
+  protected function toArray($xml) {
+    $r = array();
+
+    if (is_array($xml)) {
+      foreach ($xml as $k => $v) {
+        $r["$k"] = "$v";
+      }
+    }
+
+    return $r;
+  }
+
+  protected function assert($condition, $message = "expected(true), got(false)") {
+    if (!$condition) {
+      $this->fail($message);
+    } else {
+      $this->pass($message);
+    }
+  }
+
+  protected function assertInArray($needle, $haystack, $message = "") {
+    if (!$message) {
+      $message = "expected '$needle' in '$haystack'";
+    }
+    $this->assert(in_array($needle, $haystack), $message);
+  }
+}
---
+++ org.openiaml.model.tests/emf-tests/class.XmlLoader.php
@@ -0,0 +1,22 @@
+<?php
+
+$_singleton_instances = array();
+class XmlLoader {
+  var $_loaded = array();
+
+  public function load($url) {
+    if (!isset($this->_loaded[$url])) {
+      $this->_loaded[$url] = simplexml_load_file($url);
+    }
+
+    return $this->_loaded[$url];
+  }
+
+  static public function instance() {
+    global $_singleton_instances;
+    if (!isset($_singleton_instances["XmlLoader"])) {
+      $_singleton_instances["XmlLoader"] = new XmlLoader();
+    }
+    return $_singleton_instances["XmlLoader"];
+  }
+}

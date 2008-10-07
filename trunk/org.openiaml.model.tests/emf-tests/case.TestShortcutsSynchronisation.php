61db8f03b4e2decdec2fce4a80eb0cd87cd1ba0a
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-10-07 09:11:07 +0000

    completing a test case... considering moving to PHPunit

--- trunk/org.openiaml.model.tests/emf-tests/case.TestShortcutsSynchronisation.php
+++ trunk/org.openiaml.model.tests/emf-tests/case.TestShortcutsSynchronisation.php
@@ -1,8 +1,62 @@
 <?php
 
 class TestShortcutsSynchronisation extends GmfTestCase {
-  public function run() {
-    $xml = XmlLoader::instance()->load("root.gmfgen")->xpath("//diagram/containsShortcutsTo");
-    print_r($xml);
-  }
+	public function collect($xpath, $files) {
+	}
+
+	public function run() {
+		$files = FileFinder::instance()->match(GMF_ROOT, ".gmfgen");
+
+		$containsShortcutsTo = false;
+		{
+			$result = array();
+			$complete = array();
+			foreach ($files as $k => $f) {
+				$x = $this->toArray( XmlLoader::instance()->load($f)->xpath("//diagram/containsShortcutsTo") );
+				// add to summary
+				$result[$f] = $x;
+				// merge into complete
+				$complete = array_flip(array_merge(array_flip($complete), array_flip($x)));
+			}
+
+			// now check each $result instance to make sure they have them all
+			foreach ($result as $file => $content) {
+				foreach ($complete as $value) {
+					$this->assertInArray($value, $content, "'$file' is missing //diagram/containsShortcutsTo['$value']");
+				}
+			}
+
+			$containsShortcutsTo = $complete;
+		}
+
+		// now work out shortcutsProvidedFor and make sure they are also the same
+		$shortcutsProvidedFor = false;
+		{
+			$result = array();
+			$complete = array();
+			foreach ($files as $k => $f) {
+				$x = $this->toArray( XmlLoader::instance()->load($f)->xpath("//diagram/shortcutsProvidedFor") );
+				// add to summary
+				$result[$f] = $x;
+				// merge into complete
+				$complete = array_flip(array_merge(array_flip($complete), array_flip($x)));
+			}
+
+			// now check each $result instance to make sure they have them all
+			foreach ($result as $file => $content) {
+				foreach ($complete as $value) {
+					$this->assertInArray($value, $content, "'$file' is missing //diagram/shortcutsProvidedFor['$value']");
+				}
+			}
+
+			$shortcutsProvidedFor = $complete;
+		}
+
+		// finally, make sure they are both in sync
+		$this->assert(count($containsShortcutsTo) == count($shortcutsProvidedFor), "containsShortcutsTo is not the same length as shortcutsProvidedFor");
+		foreach ($containsShortcutsTo as $value) {
+			$this->assertInArray($value, $shortcutsProvidedFor);
+		}
+
+	}
 }

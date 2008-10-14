20d3c3d91a9f293633984a20533b425a05817b12
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-10-14 22:57:14 +0000

    can now test the take rule generation programatically with junit

--- branches/2008-10-take/org.openiaml.model.diagram.custom/src/org/openiaml/model/take/tests/MyTestCase.java
+++ branches/2008-10-take/org.openiaml.model.diagram.custom/src/org/openiaml/model/take/tests/MyTestCase.java
@@ -1,14 +1,20 @@
 package org.openiaml.model.take.tests;
 
+import iaml.generated2.InternetApplication_Children;
+import iaml.generated2.KBGenerated;
+import iaml.generated2.NamedElement_Name;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import junit.framework.TestCase;
 
-import org.eclipse.emf.ecore.EObject;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.openiaml.model.diagram.custom.commands.TestDeriveElementsCommand;
 import org.openiaml.model.model.InternetApplication;
 
 public class MyTestCase extends TestCase {
@@ -18,17 +24,33 @@ public class MyTestCase extends TestCase {
 	public void setUp() throws IOException {
 		XMIResourceImpl resource = new XMIResourceImpl();
 		File source = new File("src/org/openiaml/model/take/tests/test.iaml");
-		System.out.println(source.getCanonicalPath());
 		assertTrue("source test file exists", source.exists());
 		
 		resource.load( new FileInputStream(source), new HashMap<Object,Object>());
-		for (EObject eo : resource.getContents()) {
-			System.out.println(eo);
+		assertTrue("root of test model is an InternetApplication", resource.getContents().get(0) instanceof InternetApplication);
+		root = (InternetApplication) resource.getContents().get(0);
+	}
+	
+	public void test1() throws ExecutionException {
+		// try deriving command
+		TestDeriveElementsCommand te = new TestDeriveElementsCommand();
+		KBGenerated kb = te.executeKnowledgeBase(root);
+		for (InternetApplication_Children n : new IteratorWrapper<InternetApplication_Children>(kb.getAppChildren(root))) {
+			System.out.println("hello");
+			System.out.println(n);
 		}
 	}
 	
-	public void test1() {
-		assertTrue(true);
+	private class IteratorWrapper<T> implements Iterable<T> {
+		private Iterator<T> it;
+		public IteratorWrapper(Iterator<T> it) {
+			this.it = it;
+		}
+		
+		@Override
+		public Iterator<T> iterator() {
+			return it;
+		}
 	}
 	
 }

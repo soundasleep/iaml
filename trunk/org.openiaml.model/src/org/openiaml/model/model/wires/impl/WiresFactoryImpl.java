5b35d52e22496e8d736d2160c28ad4a37cd19b05
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-10-16 03:47:49 +0000

    all elements in the model now add ID attributes

--- trunk/org.openiaml.model/src/org/openiaml/model/model/wires/impl/WiresFactoryImpl.java
+++ trunk/org.openiaml.model/src/org/openiaml/model/model/wires/impl/WiresFactoryImpl.java
@@ -6,12 +6,15 @@
  */
 package org.openiaml.model.model.wires.impl;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.impl.EFactoryImpl;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
-import org.openiaml.model.model.wires.*;
+import org.openiaml.model.model.GeneratedElement;
 import org.openiaml.model.model.wires.CompositeWire;
 import org.openiaml.model.model.wires.ParameterWire;
 import org.openiaml.model.model.wires.RunInstanceWire;
@@ -77,50 +80,55 @@ public class WiresFactoryImpl extends EFactoryImpl implements WiresFactory {
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	public SingleWire createSingleWire() {
 		SingleWireImpl singleWire = new SingleWireImpl();
+		generateID(singleWire);
 		return singleWire;
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	public CompositeWire createCompositeWire() {
 		CompositeWireImpl compositeWire = new CompositeWireImpl();
+		generateID(compositeWire);
 		return compositeWire;
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	public SyncWire createSyncWire() {
 		SyncWireImpl syncWire = new SyncWireImpl();
+		generateID(syncWire);
 		return syncWire;
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	public RunInstanceWire createRunInstanceWire() {
 		RunInstanceWireImpl runInstanceWire = new RunInstanceWireImpl();
+		generateID(runInstanceWire);
 		return runInstanceWire;
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	public ParameterWire createParameterWire() {
 		ParameterWireImpl parameterWire = new ParameterWireImpl();
+		generateID(parameterWire);
 		return parameterWire;
 	}
 
@@ -144,4 +152,32 @@ public class WiresFactoryImpl extends EFactoryImpl implements WiresFactory {
 		return WiresPackage.eINSTANCE;
 	}
 
+	private static long generate_id_counter = 0;
+	private static final String packageDate = Long.toHexString(new Date().getTime());
+	
+	/**
+	 * We want a way to generate IDs that we know are unique between different elements
+	 * in the same model, but currently we don't mind if they aren't unique
+	 * between different models created at the exact same time. (For this, we
+	 * would need to use UUIDs.) Applies the ID to the given element.
+	 * 
+	 * This is probably a really unpleasant initial implementation but can easily
+	 * be changed in the future :)
+	 * 
+	 * Currently it sets IDs to something like "Model.12b52.42", where
+	 * - the first part is the package name
+	 * - the second part is the time the package factory was instantiated (in hex)
+	 * - the third part is a unique ID to this factory instance (in hex)
+	 * 
+	 * @see GeneratedElement
+	 * @param obj
+	 */
+	protected void generateID(EObject obj) {
+		if (obj instanceof GeneratedElement) {
+			GeneratedElement ge = (GeneratedElement) obj;			
+			generate_id_counter++;			
+			ge.setId( this.getEPackage().getName() + "." + packageDate + "." + Long.toHexString(generate_id_counter) );
+		}
+	}
+	
 } //WiresFactoryImpl

1bf4e006db9c68bdb6ebacae4fb19ae7d11a4d37
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-10-19 10:06:35 +0000

    completed implementation of the temporary inference engine

--- trunk/org.openiaml.model.inference/src/org/openiaml/model/inference/ICreateElements.java
+++ trunk/org.openiaml.model.inference/src/org/openiaml/model/inference/ICreateElements.java
@@ -3,28 +3,9 @@
  */
 package org.openiaml.model.inference;
 
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.openiaml.model.model.ApplicationElementProperty;
-import org.openiaml.model.model.ChainedOperation;
-import org.openiaml.model.model.CompositeOperation;
-import org.openiaml.model.model.ContainsWires;
-import org.openiaml.model.model.DataFlowEdge;
-import org.openiaml.model.model.DataFlowEdgeDestination;
-import org.openiaml.model.model.DataFlowEdgesSource;
-import org.openiaml.model.model.EventTrigger;
-import org.openiaml.model.model.ExecutionEdge;
-import org.openiaml.model.model.ExecutionEdgeDestination;
-import org.openiaml.model.model.ExecutionEdgesSource;
-import org.openiaml.model.model.Parameter;
-import org.openiaml.model.model.WireEdgeDestination;
-import org.openiaml.model.model.WireEdgesSource;
-import org.openiaml.model.model.operations.StartNode;
-import org.openiaml.model.model.operations.StopNode;
-import org.openiaml.model.model.visual.InputTextField;
-import org.openiaml.model.model.wires.ParameterWire;
-import org.openiaml.model.model.wires.RunInstanceWire;
-import org.openiaml.model.model.wires.SyncWire;
 
 /**
  * An interface to something that creates elements.
@@ -36,12 +17,6 @@ import org.openiaml.model.model.wires.SyncWire;
 public interface ICreateElements {
 
 	/**
-	 * @return
-	 */
-	public SyncWire createSyncWire(ContainsWires container, WireEdgesSource source,
-			WireEdgeDestination target) throws InferenceException;
-
-	/**
 	 * @param element
 	 * @param reference
 	 * @param value
@@ -50,84 +25,21 @@ public interface ICreateElements {
 	 throws InferenceException;
 
 	/**
-	 * @param field
+	 * @param container the containing element
+	 * @param elementType the element type to create
 	 * @return
 	 */
-	public ApplicationElementProperty createApplicationElementProperty(
-			InputTextField field) throws InferenceException;
-
-	/**
-	 * @param from
-	 * @return
-	 */
-	public CompositeOperation createCompositeOperation(InputTextField from);
-
-	/**
-	 * @param co
-	 * @return
-	 */
-	public StartNode createStartNode(CompositeOperation co);
-
-	/**
-	 * @param co
-	 * @return
-	 */
-	public StopNode createStopNode(CompositeOperation co);
-
-	/**
-	 * @param co
-	 * @return
-	 */
-	public ChainedOperation createChainedOperation(CompositeOperation co);
-
-	/**
-	 * @param co
-	 * @return
-	 */
-	public Parameter createParameter(CompositeOperation co);
+	EObject createElement(EObject container,
+			EClass elementType, EStructuralFeature containerFeature) throws InferenceException;
 
 	/**
 	 * @param container
-	 * @param source
-	 * @param target
-	 * @return
-	 */
-	public ExecutionEdge createExecutionEdge(CompositeOperation container,
-			ExecutionEdgesSource source, ExecutionEdgeDestination target);
-
-	/**
-	 * @param container
-	 * @param source
-	 * @param target
-	 * @return
-	 */
-	public DataFlowEdge createDataFlowEdge(CompositeOperation container,
-			DataFlowEdgesSource source, DataFlowEdgeDestination target);
-
-	/**
-	 * @param from
-	 * @return
-	 */
-	public EventTrigger createEventTrigger(InputTextField from);
-
-	/**
-	 * @param container
+	 * @param parameterWire
 	 * @param rwi
 	 * @param parameter
 	 * @return
 	 */
-	public ParameterWire createParameterWire(SyncWire container,
-			RunInstanceWire rwi, ApplicationElementProperty parameter);
-
-	/**
-	 * @param container
-	 * @param event
-	 * @param operation
-	 * @return
-	 */
-	public RunInstanceWire createRunInstanceWire(SyncWire container,
-			EventTrigger event, CompositeOperation operation);
-
-	
+	EObject createRelationship(EObject container, EClass elementType,
+			EObject source, EObject target, EStructuralFeature containerFeature, EStructuralFeature sourceFeature, EStructuralFeature targetFeature)  throws InferenceException;
 	
 }

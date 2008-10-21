66d88251496a17fcfa8f06b3375a3da6f7576acb
Jevon Wright <soundasleep@users.noreply.github.com>
2008-10-21 08:40:52 +0000

    after oaw code generation, the output project will be refreshed

--- org.openiaml.model.codegen.oaw/src/org/openiaml/model/codegen/oaw/OawCodeGenerator.java
+++ org.openiaml.model.codegen.oaw/src/org/openiaml/model/codegen/oaw/OawCodeGenerator.java
@@ -7,6 +7,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.openarchitectureware.workflow.WorkflowRunner;
@@ -23,7 +26,7 @@ public class OawCodeGenerator implements ICodeGenerator {
 	 * Generate code for a given model file into a given output directory
 	 * 
 	 */
-	public IStatus generateCode(IFile file) {
+	public IStatus generateCode(IFile file, IProgressMonitor monitor) {
 		
 		String wfFile = "src/workflow/generator.oaw";
 		Map<String,String> properties = new HashMap<String,String>();
@@ -32,6 +35,13 @@ public class OawCodeGenerator implements ICodeGenerator {
 		Map<String,Object> slotContents = new HashMap<String,Object>();
 		new WorkflowRunner().run(wfFile,
 			new NullProgressMonitor(), properties, slotContents);
+		
+		// refresh output folder
+		try {
+			file.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
+		} catch (CoreException e) {
+			return new Status(Status.WARNING, "org.openiaml.model.codegen.oaw", "Could not refresh local project", e);
+		}
 		
 		return Status.OK_STATUS;
 			

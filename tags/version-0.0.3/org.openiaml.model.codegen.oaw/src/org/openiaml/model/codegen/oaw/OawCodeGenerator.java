ae488f6a74c1fe4091ab8a5b332fb62f935c326b
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-10-23 09:28:56 +0000

    tag for version 0.0.3

---
+++ tags/version-0.0.3/org.openiaml.model.codegen.oaw/src/org/openiaml/model/codegen/oaw/OawCodeGenerator.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.openiaml.model.codegen.oaw;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.openarchitectureware.workflow.WorkflowRunner;
+import org.openarchitectureware.workflow.monitor.NullProgressMonitor;
+import org.openiaml.model.codegen.ICodeGenerator;
+
+/**
+ * @author jmwright
+ *
+ */
+public class OawCodeGenerator implements ICodeGenerator {
+	
+	/**
+	 * Generate code for a given model file into a given output directory
+	 * 
+	 */
+	public IStatus generateCode(IFile file, IProgressMonitor monitor) {
+		
+		String wfFile = "src/workflow/generator.oaw";
+		Map<String,String> properties = new HashMap<String,String>();
+		properties.put("model", file.getFullPath().toString());
+		properties.put("src-gen", file.getProject().getLocation().toString());	// have to get absolute filename for output dir
+		Map<String,Object> slotContents = new HashMap<String,Object>();
+		new WorkflowRunner().run(wfFile,
+			new NullProgressMonitor(), properties, slotContents);
+		
+		// refresh output folder
+		try {
+			file.getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
+		} catch (CoreException e) {
+			return new Status(Status.WARNING, "org.openiaml.model.codegen.oaw", "Could not refresh local project", e);
+		}
+		
+		return Status.OK_STATUS;
+			
+	}
+}

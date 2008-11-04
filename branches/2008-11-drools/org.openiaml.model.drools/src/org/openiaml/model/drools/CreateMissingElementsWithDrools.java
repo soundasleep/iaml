75faa981515e699e08dd356e69c410fe53f10757
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-11-04 05:20:16 +0000

    expanding the rules for sync wires

--- branches/2008-11-drools/org.openiaml.model.drools/src/org/openiaml/model/drools/CreateMissingElementsWithDrools.java
+++ branches/2008-11-drools/org.openiaml.model.drools/src/org/openiaml/model/drools/CreateMissingElementsWithDrools.java
@@ -3,21 +3,31 @@
  */
 package org.openiaml.model.drools;
 
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
 import org.drools.RuleBase;
 import org.drools.RuleBaseFactory;
 import org.drools.WorkingMemory;
-import org.drools.compiler.DroolsParserException;
 import org.drools.compiler.PackageBuilder;
+import org.drools.event.ObjectInsertedEvent;
+import org.drools.event.ObjectRetractedEvent;
+import org.drools.event.ObjectUpdatedEvent;
+import org.drools.event.WorkingMemoryEventListener;
 import org.drools.rule.Package;
 import org.openiaml.model.inference.ICreateElements;
+import org.openiaml.model.model.ApplicationElement;
+import org.openiaml.model.model.ApplicationElementContainer;
+import org.openiaml.model.model.ApplicationElementProperty;
+import org.openiaml.model.model.ContainsEventTriggers;
+import org.openiaml.model.model.ContainsOperations;
+import org.openiaml.model.model.ContainsWires;
+import org.openiaml.model.model.DomainStore;
+import org.openiaml.model.model.EventTrigger;
 import org.openiaml.model.model.InternetApplication;
-
-import com.sample.DroolsTest;
-import com.sample.DroolsTest.Message;
+import org.openiaml.model.model.Operation;
+import org.openiaml.model.model.StaticValue;
+import org.openiaml.model.model.WireEdge;
 
 /**
  * Uses Drools to infer new elements.
@@ -43,13 +53,99 @@ public class CreateMissingElementsWithDrools {
 
     	//load up the rulebase
         RuleBase ruleBase = readRule();
-        WorkingMemory workingMemory = ruleBase.newStatefulSession();
+        final WorkingMemory workingMemory = ruleBase.newStatefulSession();
+        
+        // automatically insert new objects based on a given object
+        workingMemory.addEventListener( new WorkingMemoryEventListener() {
+
+        	/**
+        	 * When we insert a new element, we automatically insert
+        	 * all of its children elements.
+        	 * 
+        	 * (It's probably possible to do this also in the rulebase
+        	 * itself, but this may be cleaner?)
+        	 * 
+        	 */
+			@Override
+			public void objectInserted(ObjectInsertedEvent obj) {
+				// ContainsEventTriggers
+				if (obj.getObject() instanceof ContainsEventTriggers) {
+					ContainsEventTriggers a = (ContainsEventTriggers) obj.getObject();
+					for (EventTrigger child : a.getEventTriggers()) {
+						workingMemory.insert( child );
+					}
+				}
+
+				// ContainsOperations
+				if (obj.getObject() instanceof ContainsOperations) {
+					ContainsOperations a = (ContainsOperations) obj.getObject();
+					for (Operation child : a.getOperations()) {
+						workingMemory.insert( child );
+					}
+				}
+
+				// ContainsWires
+				if (obj.getObject() instanceof ContainsWires) {
+					ContainsWires a = (ContainsWires) obj.getObject();
+					for (WireEdge child : a.getWires()) {
+						workingMemory.insert( child );
+					}
+				}
+
+				// ApplicationElement
+				if (obj.getObject() instanceof ApplicationElement) {
+					ApplicationElement a = (ApplicationElement) obj.getObject();
+					for (ApplicationElementProperty child : a.getProperties()) {
+						workingMemory.insert( child );
+					}
+					for (StaticValue child : a.getValues()) {
+						workingMemory.insert( child );
+					}
+				}
+
+				// ApplicationElementContainer
+				if (obj.getObject() instanceof ApplicationElementContainer) {
+					ApplicationElementContainer a = (ApplicationElementContainer) obj.getObject();
+					for (ApplicationElement child : a.getChildren()) {
+						workingMemory.insert( child );
+					}
+				}
+				
+				// InternetApplication
+				if (obj.getObject() instanceof InternetApplication) {
+					InternetApplication a = (InternetApplication) obj.getObject();
+					for (ApplicationElementProperty child : a.getProperties()) {
+						workingMemory.insert( child );
+					}
+					for (ApplicationElement child : a.getChildren()) {
+						workingMemory.insert( child );
+					}
+					for (DomainStore child : a.getDomainStores()) {
+						workingMemory.insert( child );
+					}
+				}
+
+			}
+
+			@Override
+			public void objectRetracted(ObjectRetractedEvent x) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void objectUpdated(ObjectUpdatedEvent x) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
+        });
         
         //go !
         workingMemory.insert( root );
         workingMemory.insert( handler );
         workingMemory.fireAllRules();   
-
+        
 	}
 
 	/**
@@ -61,7 +157,7 @@ public class CreateMissingElementsWithDrools {
 	private RuleBase readRule() throws Exception {
 
 		//read in the source
-		Reader source = new InputStreamReader( DroolsTest.class.getResourceAsStream( "/Testrules.drl" ) );
+		Reader source = new InputStreamReader( CreateMissingElementsWithDrools.class.getResourceAsStream( "/Testrules.drl" ) );
 		
 		//optionally read in the DSL (if you are using it).
 		//Reader dsl = new InputStreamReader( DroolsTest.class.getResourceAsStream( "/mylang.dsl" ) );

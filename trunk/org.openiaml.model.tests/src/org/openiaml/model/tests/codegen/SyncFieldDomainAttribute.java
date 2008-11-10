53b0e6643c29b3cffe1d295f0c6b9077e4aea061
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-11-10 01:23:59 +0000

    adding codegen code to generate domain objects in sqlite upgrading jwebunit to latest trunk version

--- trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/codegen/SyncFieldDomainAttribute.java
+++ trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/codegen/SyncFieldDomainAttribute.java
@@ -19,6 +19,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.ecore.EObject;
 import org.openiaml.model.drools.CreateMissingElementsWithDrools;
 import org.openiaml.model.inference.EcoreInferenceHandler;
+import org.openiaml.model.model.DomainStore;
 import org.openiaml.model.model.InternetApplication;
 import org.openiaml.model.tests.InferenceTestCase;
 
@@ -58,10 +59,14 @@ public class SyncFieldDomainAttribute extends InferenceTestCase {
 	 * through SQLite)
 	 */
 	public void testDatabaseIsEmpty() throws Exception {
+		// get the DomainStore in this app
+		DomainStore ds = (DomainStore) queryOne(root, "iaml:domainStores[iaml:name='store']");
+		String dbName = "output/" + safeName(ds) + ".db";  
+
 		// there should first be no database
 		{
-			IFile db = getProject().getFile("output/test.db");
-			assertFalse("db shouldn't exist", db.exists());
+			IFile db = getProject().getFile(dbName);
+			assertFalse("db '" + dbName + "' shouldn't exist", db.exists());
 		}
 
 		// we can now go to sitemap
@@ -73,19 +78,58 @@ public class SyncFieldDomainAttribute extends InferenceTestCase {
 
 		// refresh the workspace
 		assertTrue(refreshProject().isOK());
-
+		
 		// the database should now exist 
-		IFile db = getProject().getFile("output/test.db");
-		assertTrue("db should exist now that we have initialised the application", db.exists());
+		IFile db = getProject().getFile(dbName);
+		assertTrue("db '" + dbName + "' should exist now that we have initialised the application", db.exists());
 
 		// make sure the database is empty
-		Class.forName("org.sqlite.JDBC");
-		Connection conn = DriverManager.getConnection("jdbc:sqlite:" + db.getLocation());
-		Statement stat = conn.createStatement();
+		{
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:" + db.getLocation());
+			Statement stat = conn.createStatement();
+	
+			ResultSet rs = stat.executeQuery("SELECT attribute FROM domain;");
+			assertFalse(rs.next());	// it should be empty
+		}
+		
+		// we should now be able to fill in a field
+		// and the result will stay the same when we reload the page
+		goSitemapThenPage(sitemap, "page");
+		String fieldID = getLabelIDForText("single-text-field");
+		String testingText = new Date().toString();
+		
+		// it should start out as empty
+		assertLabeledFieldEquals(fieldID, "");
+		
+		// set it
+		setLabeledFormElementField(fieldID, testingText);
+		
+		// reload the page and make sure it's the same
+		// TODO add reloadPage() to JWebUnit
+		goSitemapThenPage(sitemap, "page");
+		assertLabeledFieldEquals(fieldID, testingText);
+		
+		// and again
+		goSitemapThenPage(sitemap, "page");
+		assertLabeledFieldEquals(fieldID, testingText);
 
-		ResultSet rs = stat.executeQuery("select field1 from form1;");
-		assertFalse(rs.next());	// it should be empty
-
+		// check the database
+		{
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:" + db.getLocation());
+			Statement stat = conn.createStatement();
+	
+			ResultSet rs = stat.executeQuery("SELECT attribute FROM domain;");
+			assertTrue(rs.next());	// there should be one value
+			
+			// it should be our testing text
+			assertEquals(rs.getString("field1"), testingText);
+			
+			// there should not be any more values in the database
+			assertFalse(rs.next());
+		}
+		
 	}
 		
 	/**

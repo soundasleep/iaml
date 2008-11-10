646cf3668cb70d0d4d6eef4bbbe3bfa3a3182bc6
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-11-10 03:57:06 +0000

    added a complicated SyncWire example which connects multiple forms and DB objects together added very limited loop-prevention code on the server side

---
+++ trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/codegen/SyncWiresMultiple.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package org.openiaml.model.tests.codegen;
+
+import java.util.Date;
+import java.util.Random;
+
+import org.eclipse.core.resources.IFile;
+import org.openiaml.model.model.InternetApplication;
+import org.openiaml.model.tests.CodegenTestCase;
+
+/**
+ * Testing a big complicated sync wires example.
+ * 
+ * page1 <--> page2 <--> user form <--> signup form <--> last signup user
+ * 
+ * @author jmwright
+ *
+ */
+public class SyncWiresMultiple extends CodegenTestCase {
+	
+	protected InternetApplication root;
+	
+	protected void setUp() throws Exception {
+		root = loadAndCodegen(ROOT + "codegen/SyncWiresMultiple.iaml");
+	}
+	
+	public void testCase() throws Exception {
+		// go to sitemap
+		IFile sitemap = getProject().getFile("output/sitemap.html");
+		assertTrue("sitemap " + sitemap + " exists", sitemap.exists());
+		
+		// go to page1
+		goSitemapThenPage(sitemap, "page1");
+		
+		// fill in name and email
+		String testName = "name " + new Date().toString();
+		String testEmail = "email " + new Random().nextInt(32768);
+		String testPassword = "password " + new Date().toString();
+		{
+			String nameId = getLabelIDForText("name");
+			assertNotNull(nameId);
+			setLabeledFormElementField(nameId, testName);
+			String emailId = getLabelIDForText("email");
+			assertNotNull(emailId);
+			setLabeledFormElementField(emailId, testEmail);
+		}
+		
+		// go to page2
+		goSitemapThenPage(sitemap, "page2");
+		
+		// should have changed
+		{
+			String nameId = getLabelIDForText("name");
+			assertLabeledFieldEquals(nameId, testName);
+			String emailId = getLabelIDForText("email");
+			assertLabeledFieldEquals(emailId, testEmail);
+		}
+		
+		// set password
+		{
+			String passwordId = getLabelIDForText("password");
+			setLabeledFormElementField(passwordId, testPassword);
+		}
+		
+		// if we now go to the last signup user page, it should appear there
+		goSitemapThenPage(sitemap, "last signup user");
+		
+		// should have changed
+		{
+			String nameId = getLabelIDForText("name");
+			assertLabeledFieldEquals(nameId, testName);
+		}
+		
+	}
+
+}

f44a17c86dc420e3d765b46efae2f0c1a59b4291
Jevon Wright <soundasleep@users.noreply.github.com>
2008-11-11 00:19:21 +0000

    refactoring and cleaning up eclipse test cases

---
+++ org.openiaml.model.tests/src/org/openiaml/model/tests/eclipse/LoadDiagramTestCase.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package org.openiaml.model.tests.eclipse;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeNodeEditPart;
+import org.eclipse.ui.IEditorPart;
+import org.openiaml.model.model.diagram.part.IamlDiagramEditor;
+import org.openiaml.model.tests.EclipseTestCaseHelper;
+
+/**
+ * Test loading a diagram in Eclipse.
+ * 
+ * @author jmwright
+ *
+ */
+public class LoadDiagramTestCase extends EclipseTestCaseHelper {
+
+	public void testLoadModel() throws Exception {
+		// copy our local file into the project
+		IFile targetModel = project.getFile("generation-sync-multiple.iaml");
+		copyFileIntoWorkspace("src/org/openiaml/model/tests/eclipse/generation-sync-multiple.iaml",
+				targetModel);
+		IFile targetDiagram = project.getFile("generation-sync-multiple.iaml_diagram");
+		copyFileIntoWorkspace("src/org/openiaml/model/tests/eclipse/generation-sync-multiple.iaml_diagram",
+				targetDiagram);
+
+		// load up the editor
+		IEditorPart ep = loadDiagramFile(targetDiagram);
+
+		// if this is actually an ErrorEditPart, then an error has occured 
+		// (but it may not be obvious in the log what it is)
+		assertTrue("active editor is our plugin, but is " + ep, ep instanceof IamlDiagramEditor);
+		
+		// find what elements are displayed
+		IamlDiagramEditor editor = (IamlDiagramEditor) ep;
+
+		// there should be four children
+		assertEquals("there should only be 4 children", 4, editor.getDiagramEditPart().getChildren().size());
+		
+		// check the contents
+		ShapeNodeEditPart page1 = assertHasRootPage(editor, "page1");
+		ShapeNodeEditPart page2 = assertHasRootPage(editor, "page2");
+		ShapeNodeEditPart store = assertHasRootDomainStore(editor, "domainStore");
+		ShapeNodeEditPart page4 = assertHasRootPage(editor, "last signup user");
+
+	}
+
+	
+}

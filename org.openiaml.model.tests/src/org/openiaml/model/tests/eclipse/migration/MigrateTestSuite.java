47fa8e89db67eeaf181f9be6706d0d96c57c3d63
Jevon Wright <soundasleep@users.noreply.github.com>
2008-11-12 02:18:42 +0000

    refactoring of the test cases: renaming abstract test case helpers adding migration test suite

---
+++ org.openiaml.model.tests/src/org/openiaml/model/tests/eclipse/migration/MigrateTestSuite.java
@@ -0,0 +1,27 @@
+package org.openiaml.model.tests.eclipse.migration;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * All tests for the Migration functionality.
+ * 
+ * @author jmwright
+ *
+ */
+public class MigrateTestSuite {
+
+	/**
+	 * Get all the tests in this package and return as a test suite.
+	 * 
+	 * @return
+	 */
+    public static Test suite() { 
+        TestSuite suite = new TestSuite("Migration");
+
+        suite.addTestSuite(MigrateSignupForm.class);
+
+        return suite; 
+   }
+
+}

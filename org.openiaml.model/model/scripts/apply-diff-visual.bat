671c43c3144e3e7ccee590f60cf09c7177d8bc70
Jevon Wright <soundasleep@users.noreply.github.com>
2008-12-09 03:57:06 +0000

    adding diffs for all other gmfgens

--- org.openiaml.model/model/scripts/apply-diff-visual.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem -- apply the diff for visual model .gmfgen --
-
-cd ..
-patch visual.gmfgen diff/visual.gmfgen.diff
--- org.openiaml.model/model/scripts/make-diff-visual.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem -- create diff's for visual model .gmfgen --
-
-cd ..
-
-mkdir diff
-
-diff -u visual.gmfgen copies/visual.gmfgen > diff/visual.gmfgen.diff

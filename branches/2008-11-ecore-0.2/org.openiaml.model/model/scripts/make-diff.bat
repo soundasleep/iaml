ca58e258548d04f07acb3077a816465edb8786bf
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-12-09 04:02:07 +0000

    correcting diff for visual gmfgen

--- branches/2008-11-ecore-0.2/org.openiaml.model/model/scripts/make-diff.bat
+++ branches/2008-11-ecore-0.2/org.openiaml.model/model/scripts/make-diff.bat
@@ -3,5 +3,5 @@ rem -- create diff's for %1 model .gmfgen --
 
 cd ..
 mkdir diff
-diff -u %1.gmfgen copies/%1.gmfgen > diff/%1.gmfgen.diff
+diff -u -w %1.gmfgen copies/%1.gmfgen > diff/%1.gmfgen.diff
 cd scripts

83463c4a03955316c4620304a6722344da449292
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2008-12-15 22:19:12 +0000

    adding exception text to CancelNode

--- branches/2008-11-ecore-0.2/org.openiaml.model/src/org/openiaml/model/model/operations/CancelNode.java
+++ branches/2008-11-ecore-0.2/org.openiaml.model/src/org/openiaml/model/model/operations/CancelNode.java
@@ -14,10 +14,42 @@ import org.openiaml.model.model.ExecutionEdgeDestination;
  * A representation of the model object '<em><b>Cancel Node</b></em>'.
  * <!-- end-user-doc -->
  *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.openiaml.model.model.operations.CancelNode#getExceptionText <em>Exception Text</em>}</li>
+ * </ul>
+ * </p>
  *
  * @see org.openiaml.model.model.operations.OperationsPackage#getCancelNode()
  * @model
  * @generated
  */
 public interface CancelNode extends ActivityNode, ExecutionEdgeDestination {
+
+	/**
+	 * Returns the value of the '<em><b>Exception Text</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Exception Text</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Exception Text</em>' attribute.
+	 * @see #setExceptionText(String)
+	 * @see org.openiaml.model.model.operations.OperationsPackage#getCancelNode_ExceptionText()
+	 * @model
+	 * @generated
+	 */
+	String getExceptionText();
+
+	/**
+	 * Sets the value of the '{@link org.openiaml.model.model.operations.CancelNode#getExceptionText <em>Exception Text</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Exception Text</em>' attribute.
+	 * @see #getExceptionText()
+	 * @generated
+	 */
+	void setExceptionText(String value);
 } // CancelNode

c0c2970be31120d6b4493639ba33b901a84c8d73
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2009-01-29 09:43:12 +0000

    refactoring migration code: creating a new DomBasedMigrator abstract class

--- trunk/org.openiaml.model.diagram.custom/src/org/openiaml/model/diagram/custom/migrate/Migrate0To1.java
+++ trunk/org.openiaml.model.diagram.custom/src/org/openiaml/model/diagram/custom/migrate/Migrate0To1.java
@@ -1,14 +1,7 @@
 package org.openiaml.model.diagram.custom.migrate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -23,7 +16,7 @@ import org.w3c.dom.Node;
  * @author jmwright
  *
  */
-public class Migrate0To1 implements IamlModelMigrator {
+public class Migrate0To1 extends DomBasedMigrator implements IamlModelMigrator {
 
 	public String getName() {
 		return "Migrator 0.0 to 0.1";
@@ -52,298 +45,97 @@ public class Migrate0To1 implements IamlModelMigrator {
 				// this is us! (version 0.0)
 				return true;
 			}
-			
-			// always return true for now TODO remove me!
-			if (true)
-				return true;
 		} catch (Exception e) {
 			throw new MigrationException(e);
 		}
 			
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	/* (non-Javadoc)
-	 * @see org.openiaml.model.diagram.custom.actions.MigrateModelAction.IamlModelMigrator#migrate(org.eclipse.core.resources.IFile, org.eclipse.core.resources.IFile, org.eclipse.core.runtime.IProgressMonitor)
+	 * @see org.openiaml.model.diagram.custom.migrate.DomBasedMigrator#getRenamedNode(java.lang.String, org.w3c.dom.Element)
 	 */
 	@Override
-	public Document migrate(Document inputDoc, IProgressMonitor monitor, List<ExpectedMigrationException> errors)
-			throws MigrationException {
-		try {
-			// create the new document (output)
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document newDoc = db.newDocument();
-			
-			// cycle through each element and add "id" attributes
-			// and map their "id" attribute to their original position
-			idMap = new HashMap<String,String>();
-			recurseOverDocument(inputDoc, newDoc, errors);
-			
-			// we now have our new model
-			return newDoc;
-
-		} catch (Exception e) {
-			throw new MigrationException(e);
-		}
-	}
-
-	/**
-	 * @see #removeDeletedReferences(Element)
-	 */
-	protected List<String> deletedIds;
-	
-	/**
-	 * Do the model migration, and then remove referenced
-	 * element attributes that have been deleted during the
-	 * migration process.
-	 * 
-	 * @see #recurseOverDocument(Element, Node, Document)
-	 * @see #removeDeletedReferences(Element)
-	 * @param input
-	 * @param output
-	 */
-	protected void recurseOverDocument(Document input, Document output, List<ExpectedMigrationException> errors) {
-		// reset deleted list
-		deletedIds = new ArrayList<String>();
-		
-		// recurse over the document, adding elements and translating
-		// where necessary
-		recurseOverDocument( input.getDocumentElement(), output, output, errors );
-		
-		// go over all deleted elements and remove them from all
-		// referenced attributes
-		removeDeletedReferences(output.getDocumentElement());
-	}
-	
-	/**
-	 * We need to be able to handle removed references. Because
-	 * a reference may be deleted either at the start or end of the document,
-	 * and before or after we have translated any references to it,
-	 * we must remove deleted references <em>after</em> the translation
-	 * process has been completed.
-	 * 
-	 * This may be quite slow.
-	 * 
-	 * @param element
-	 */
-	protected void removeDeletedReferences(Element element) {
-		for (String id : deletedIds) {
-			for (int i = 0; i < element.getAttributes().getLength(); i++) {
-				String attrName = element.getAttributes().item(i).getNodeName();
-				String attrValue = element.getAttributes().item(i).getNodeValue();
-				
-				// does this attribute have it?
-				if (attrValue.equals(id)) {
-					element.removeAttribute(attrName);
-				} else if (attrValue.indexOf(id + " ") == 0) {
-					// starts with it
-					element.setAttribute(attrName, attrValue.substring( attrValue.indexOf(id + " " )));
-				} else if (attrValue.indexOf(" " + id) > 0) {
-					// ends with it
-					element.setAttribute(attrName, attrValue.substring( 0, attrValue.indexOf(" " + id )));
-				}
-				
-			}
-		}
-		
-		// process over child nodes
-		for (int i = 0; i < element.getChildNodes().getLength(); i++) {
-			Node n = element.getChildNodes().item(i);
-			if (n instanceof Element) {
-				removeDeletedReferences((Element) n);
-			}
-		}
-	}
-	
-	/**
-	 * Calculate the old and new IDs for this element, add the
-	 * mapping (for future references), and add it as a deleted reference.
-	 * 
-	 * @param element
-	 */
-	protected void addDeletedReference(Element element) {
-		String oldId = calculateOldId(element);
-		String newId = getMigratedId(oldId);
-		idMap.put(oldId, newId);
-		deletedIds.add(newId);
-	}
-
-	/**
-	 * For generating new migration IDs
-	 */
-	private int idCounter = 0;
-	
-	/**
-	 * Here we recurse over a document, doing the actual migration.
-	 * We can rename, remove or add nodes, and change attributes or
-	 * types (xsi:type).
-	 * 
-	 * <b>NOTE:</b> This is where all the logic for migration is stored.
-	 * 
-	 * @param element
-	 * @param output
-	 * @param document
-	 */
-	protected void recurseOverDocument(Element element,
-			Node output, Document document, List<ExpectedMigrationException> errors) {
-		String nodeName = element.getNodeName();
-		
+	public String getRenamedNode(String nodeName, Element element, List<ExpectedMigrationException> errors) {
 		// <edges> --> <wires>
-		if (element.getNodeName().equals("edges")) {
-			nodeName = "wires";
+		if (nodeName.equals("edges")) {
+			return "wires";
 		}
-		Element e = document.createElement( nodeName );
 		
-		// an <operation> can no longer contain <wires>
-		if (element.getNodeName().equals("edges") && output.getNodeName().equals("operations")) {
-			errors.add( new ExpectedMigrationException( this, element, "<operation> can no longer contain <edges>" ) );
+		return nodeName;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openiaml.model.diagram.custom.migrate.DomBasedMigrator#shouldDeleteNode(org.w3c.dom.Element, org.w3c.dom.Node)
+	 */
+	@Override
+	public boolean shouldDeleteNode(Element element, Node parent, List<ExpectedMigrationException> errors) {
+		// an <operation> can no longer contain <wires> (<edges>)
+		if (element.getNodeName().equals("edges") && parent.getNodeName().equals("operations")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openiaml.model.diagram.custom.migrate.DomBasedMigrator#handleAttribute(java.lang.String, java.lang.String, org.w3c.dom.Element)
+	 */
+	@Override
+	public String handleAttribute(String name, String value, Element element, List<ExpectedMigrationException> errors) {
+		// should we replace IDs?
+		if (value.indexOf("//@") >= 0) {
+			value = replaceReferences(value);
+		}
+
+		return value;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openiaml.model.diagram.custom.migrate.DomBasedMigrator#replaceType(org.w3c.dom.Element, java.lang.String)
+	 */
+	@Override
+	public String replaceType(Element element, String xsiType, List<ExpectedMigrationException> errors) {
+		// RunWire --> RunInstanceWire
+		if (xsiType.equals("iaml.wires:RunWire")) {
+			xsiType = "iaml.wires:RunInstanceWire";
+		}
+
+		// ProvidedParameterWire --> ParameterWire
+		if (xsiType.equals("iaml.wires:ProvidedParameterWire")) {
+			xsiType = "iaml.wires:ParameterWire";
+		}
+
+		// PropertyToExecutionWire has been removed
+		if (xsiType.equals("iaml.wires:PropertyToExecutionWire")) {
+			errors.add( new ExpectedMigrationException( this, element, "Type PropertyToExecutionWire no longer exists" ) );
 			addDeletedReference(element);
-			return;
+			return null;
 		}
 		
-		// cycle over attributes
-		for (int i = 0; i < element.getAttributes().getLength(); i++) {
-			Node n = element.getAttributes().item(i);
-			String value = n.getNodeValue();
-			
-			// should we replace IDs?
-			if (value.indexOf("//@") >= 0) {
-				value = replaceReferences(value);
-			}
-			
-			e.setAttribute( n.getNodeName(), value );
-		}
-		
-		// does it have an id?
-		if (element.getAttribute("id").isEmpty()) {
-			String oldId = calculateOldId(element);
-			String newId = getMigratedId(oldId);
-			idMap.put(oldId, newId);
-			e.setAttribute("id", newId);
-		}
-		
-		// should we replace the xsi:type?
-		if (!element.getAttribute("xsi:type").isEmpty()) {
-			// RunWire --> RunInstanceWire
-			String xsiType = element.getAttribute("xsi:type");
-			if (xsiType.equals("iaml.wires:RunWire")) {
-				xsiType = "iaml.wires:RunInstanceWire";
-			}
-
-			// ProvidedParameterWire --> ParameterWire
-			if (xsiType.equals("iaml.wires:ProvidedParameterWire")) {
-				xsiType = "iaml.wires:ParameterWire";
-			}
-
-			// PropertyToExecutionWire has been removed
-			if (xsiType.equals("iaml.wires:PropertyToExecutionWire")) {
-				errors.add( new ExpectedMigrationException( this, element, "Type PropertyToExecutionWire no longer exists" ) );
-				addDeletedReference(element);
-				return;
-			}
-			
-			// we can no longer have operations that are StartNodes:
-			// replace with a normal operation
-			if (element.getNodeName().equals("operations")) {
-				if (xsiType.equals("iaml.operations:StartNode") ||
-						xsiType.equals("iaml.operations:StopNode") ||
-						xsiType.equals("iaml.operations:FinishNode")) {
-					xsiType = "iaml:ChainedOperation";
-					// we can't add an attribute that isn't in the EMF model
-					// e.setAttribute("migratedWarning", "operations can no longer be StartNode, StopNode or FinishNode");
-					errors.add( new ExpectedMigrationException( this, element, "Operations can no longer be of type StartNode, StopNode or FinishNode; operation changed to ChainedOperation" ) );
-				}
-			}
-
-			e.setAttribute("xsi:type", xsiType);
-		}
-		
-		// recurse over children
-		for (int i = 0; i < element.getChildNodes().getLength(); i++) {
-			Node n = element.getChildNodes().item(i);
-			if (n.getNodeType() == Node.ELEMENT_NODE) {
-				recurseOverDocument((Element) n, e, document, errors);
-			} else {					
-				// e.appendChild(document.adoptNode(n));	// this also adds the children of the node, which we don't want 
+		// we can no longer have operations that are StartNodes:
+		// replace with a normal operation
+		if (element.getNodeName().equals("operations")) {
+			if (xsiType.equals("iaml.operations:StartNode") ||
+					xsiType.equals("iaml.operations:StopNode") ||
+					xsiType.equals("iaml.operations:FinishNode")) {
+				xsiType = "iaml:ChainedOperation";
+				// we can't add an attribute that isn't in the EMF model
+				// e.setAttribute("migratedWarning", "operations can no longer be StartNode, StopNode or FinishNode");
+				errors.add( new ExpectedMigrationException( this, element, "Operations can no longer be of type StartNode, StopNode or FinishNode; operation changed to ChainedOperation" ) );
 			}
 		}
 		
-		output.appendChild(e);
-	}
-	
-	/**
-	 * Replace "//@foo.1/@bar.2 //@car.3" with "id1 id2"
-	 * 
-	 * @param value
-	 * @return
-	 */
-	private String replaceReferences(String value) {
-		// multiple references in here?
-		String[] references = value.split(" ");
-		String newValue = "";
-		for (String ref : references) {
-			newValue += getMigratedId(ref) + " ";
-		}
-		return newValue.trim();
+		return xsiType;
 	}
 
-	/**
-	 * Calculate what would be the old ID for this element.
-	 * 
-	 * @param element
-	 * @return
+	/* (non-Javadoc)
+	 * @see org.openiaml.model.diagram.custom.migrate.DomBasedMigrator#handleElement(org.w3c.dom.Element, org.w3c.dom.Element, java.util.List)
 	 */
-	private String calculateOldId(Element element) {
-		Node n = element.getParentNode();
-		if (n == null || n instanceof Document || !(n instanceof Element)) {
-			return "/";		// root
-		}
-		return calculateOldId((Element) n) + "/@" + element.getNodeName() + "." + getNodeIndex(element);
-	}
-	
-	
-	/**
-	 * Get the migrated ID for this element. In some cases the element
-	 * will be referenced before the "id" attribute can be added;
-	 * in others, the references will occur after the "id" attribute
-	 * has been added.
-	 * 
-	 * @param oldId
-	 * @return
-	 */
-	private String getMigratedId(String oldId) {
-		if (idMap.containsKey(oldId)) {
-			return idMap.get(oldId);
-		}
-		String newId = "migrated" + idCounter++;
-		idMap.put(oldId, newId);
-		return newId;
+	@Override
+	public void handleElement(Element old, Element element,
+			List<ExpectedMigrationException> errors) {
+		// does nothing
 	}
 
-	/**
-	 * @param element
-	 * @return
-	 */
-	private int getNodeIndex(Element element) {
-		Node n = element.getParentNode();
-		int count = 0;
-		for (int i = 0; i < n.getChildNodes().getLength(); i++) {
-			Node nn = n.getChildNodes().item(i);
-			if (nn.getNodeName().equals(element.getNodeName())) {
-				if (nn.equals(element))
-					return count;
-				count++;
-			}
-		}
-		// should never get this far
-		throw new RuntimeException("Could not find frequency of node '" + element + "'");
-	}
-
-	/**
-	 * A map of old IDs ("//@foo.0") to new IDs ("foo1")
-	 */
-	protected Map<String,String> idMap;
-	
 }

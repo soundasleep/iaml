0e8fb6567be5ffa6f8e0c3da3a3931a9547402cd
Jevon Wright <soundasleep@users.noreply.github.com>
2009-02-02 23:34:11 +0000

    SyncWires now generate InputTextFields on forms if they have no elements and are connected to AbstractDomainObjects

--- org.openiaml.model.tests/src/org/openiaml/model/tests/inference/SyncWireProperties.java
+++ org.openiaml.model.tests/src/org/openiaml/model/tests/inference/SyncWireProperties.java
@@ -6,20 +6,14 @@ package org.openiaml.model.tests.inference;
 import java.util.List;
 
 import org.jaxen.JaxenException;
-import org.openiaml.model.model.ApplicationElementProperty;
-import org.openiaml.model.model.DomainAttribute;
-import org.openiaml.model.model.DomainObject;
-import org.openiaml.model.model.EventTrigger;
 import org.openiaml.model.model.InternetApplication;
-import org.openiaml.model.model.Operation;
+import org.openiaml.model.model.NamedElement;
 import org.openiaml.model.model.domain.FileDomainAttribute;
 import org.openiaml.model.model.domain.FileDomainObject;
 import org.openiaml.model.model.domain.FileDomainStore;
 import org.openiaml.model.model.visual.InputForm;
 import org.openiaml.model.model.visual.InputTextField;
 import org.openiaml.model.model.visual.Page;
-import org.openiaml.model.model.wires.ParameterWire;
-import org.openiaml.model.model.wires.RunInstanceWire;
 import org.openiaml.model.model.wires.SyncWire;
 import org.openiaml.model.tests.InferenceTestCase;
 
@@ -48,9 +42,9 @@ public class SyncWireProperties extends InferenceTestCase {
 		// there should be exactly three fields here (generated)
 		List<?> nodes = query(form, "iaml:children");
 		assertEquals(3, nodes.size());
-		InputTextField f1 = (InputTextField) nodes.get(0);		
-		InputTextField f2 = (InputTextField) nodes.get(0);		
-		InputTextField f3 = (InputTextField) nodes.get(0);
+		InputTextField f1 = (InputTextField) getNodeWithName(nodes, "fruit");
+		InputTextField f2 = (InputTextField) getNodeWithName(nodes, "animal");
+		InputTextField f3 = (InputTextField) getNodeWithName(nodes, "country");
 		assertEquals(f1.getName(), "fruit");
 		assertTrue(f1.isIsGenerated());
 		assertEquals(f2.getName(), "animal");
@@ -59,17 +53,17 @@ public class SyncWireProperties extends InferenceTestCase {
 		assertTrue(f3.isIsGenerated());
 		
 		// find the sync wire
-		SyncWire wire = (SyncWire) queryOne(page, "//iaml:wires[iaml:name='sync']");
+		SyncWire wire = (SyncWire) queryOne(page, "//iaml:wires[iaml:name='sync properties']");
 		assertNotNull(wire);
 		assertEquals(wire.getFrom(), form);
 		assertEquals(wire.getTo(), properties);
 
 		// there should be exactly 3 attributes
-		List<?> nodes2 = query(properties, "iaml.domain:children");
+		List<?> nodes2 = query(properties, "iaml.domain:attributes");
 		assertEquals(3, nodes2.size());
-		FileDomainAttribute a1 = (FileDomainAttribute) nodes2.get(0);		
-		FileDomainAttribute a2 = (FileDomainAttribute) nodes2.get(0);		
-		FileDomainAttribute a3 = (FileDomainAttribute) nodes2.get(0);
+		FileDomainAttribute a1 = (FileDomainAttribute) getNodeWithName(nodes2, "fruit");		
+		FileDomainAttribute a2 = (FileDomainAttribute) getNodeWithName(nodes2, "animal");		
+		FileDomainAttribute a3 = (FileDomainAttribute) getNodeWithName(nodes2, "country");
 		assertEquals(a1.getName(), "fruit");
 		assertEquals(a2.getName(), "animal");
 		assertEquals(a3.getName(), "country");
@@ -82,6 +76,15 @@ public class SyncWireProperties extends InferenceTestCase {
 		assertTrue(sw1.isIsGenerated());
 		assertTrue(sw2.isIsGenerated());
 		assertTrue(sw3.isIsGenerated());
+	}
+	
+	protected NamedElement getNodeWithName(List<?> nodes, String name) {
+		for (Object n : nodes) {
+			if (n instanceof NamedElement && name.equals(((NamedElement) n).getName()))
+				return (NamedElement) n;
+		}
+		fail("No node found with name '" + name + "'");
+		return null;
 	}
 	
 }

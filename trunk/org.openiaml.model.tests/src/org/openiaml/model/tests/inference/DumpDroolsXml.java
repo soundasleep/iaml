52acb22b7d282300c17ee40560ed2dde562f35a3
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2009-03-17 03:27:28 +0000

    drools rules dumping can now access the XML directly

--- trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/inference/DumpDroolsXml.java
+++ trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/inference/DumpDroolsXml.java
@@ -3,13 +3,42 @@
  */
 package org.openiaml.model.tests.inference;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
+import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.eclipse.core.resources.IFile;
 import org.openiaml.model.drools.DroolsXmlDumper;
 import org.openiaml.model.tests.InferenceTestCase;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
 
 /**
  * Dump the XML involved in the rule bases.
@@ -29,21 +58,159 @@ public class DumpDroolsXml extends InferenceTestCase {
 	
 		for (String f : results.keySet()) {
 			
+			String name = f.substring(f.lastIndexOf("/"));
+			IFile out = project.getFile(name + ".xml");
+
 			// who knows what format XmlDump is supplied in?
 			// we will assume UTF-8 as the dumped XML is UTF-8
 			InputStream source = new ByteArrayInputStream(results.get(f).getBytes("UTF-8")); 
 
-			//IFile out = project.getFile(f.replaceAll("^[A-Za-z0-9_\\-]", "") + ".xml");
-			String name = f.substring(f.lastIndexOf("/"));
-			IFile out = project.getFile(name + ".xml");
-			//project.getFolder(out.getProjectRelativePath()).create(true, true, monitor);
+			System.out.println("Writing " + out + "...");
 			
-			System.out.println("Wrote " + out);
-			out.create(source, true, monitor);
+			// load the created XML and replace the <rhs> with 
+			// more XML (specific to our use in IAML)
+			
+			Document d = loadDocument(source);
+			NodeList rhs = xpath(d, "//rhs");
+			
+			for (int i = 0; i < rhs.getLength(); i++) {
+				Text n = (Text) rhs.item(i).getFirstChild();
+				System.out.println(n.getData());
+				n.setData("testing");	// create the new nodes in here
+			}
+			
+			// out.create(source, true, monitor);
+			saveDocument(d, out.getLocation().toFile());
 
 		}
 		
 		refreshProject();
 	}
+	
+	/**
+	 * Helper methods, copied directly from XmlTestCase. TODO move these out into a different class.
+	 */
 
+	/**
+	 * Apply an XPath query to an XML document.
+	 */
+	public NodeList xpath(Node doc, String query) throws XPathExpressionException {
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+		XPathExpression expr = xpath.compile(query);
+		NodeList result = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+		return result;
+	}
+	
+	/**
+	 * Get the first node result from an XPath query.
+	 */
+	public Element xpathFirst(Document doc, String query) throws XPathExpressionException {
+		return (Element) xpath(doc, query).item(0);
+	}
+	
+	public Document firstDocument(Map<?,Document> map) {
+		return map.values().iterator().next();
+	}
+	
+	/**
+	 * Load a properties file.
+	 */
+	public Properties loadProperties(String manifest) throws FileNotFoundException, IOException {
+		Properties p = new Properties();
+		p.load(new FileInputStream(manifest));
+		return p;
+	}
+	
+	/**
+	 * Load an XML document.
+	 */
+	public Document loadDocument(String filename) throws FileNotFoundException, ParserConfigurationException, SAXException, IOException {
+		return loadDocument( new FileInputStream(filename) );
+	}
+
+	/**
+	 * Load an XML document.
+	 */
+	public Document loadDocument(InputStream source) throws ParserConfigurationException, SAXException, IOException {
+		// load the model version
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.parse(source);
+		
+		// done
+		source.close();
+		
+		return doc;
+	}
+
+	/**
+	 * Try saving an XML document.
+	 */
+	public void saveDocument(Document doc, File target) throws IOException, TransformerException {
+        TransformerFactory transfac = TransformerFactory.newInstance();
+        Transformer trans = transfac.newTransformer();
+        trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");	// omit '<?xml version="1.0"?>'
+        trans.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        // TODO clean this up into a piped input/output stream setup?
+		FileWriter sw = new FileWriter(target);
+        StreamResult result = new StreamResult(sw);
+        DOMSource source = new DOMSource(doc);
+        trans.transform(source, result);
+        sw.close();
+	}
+	
+	/**
+	 * Assert that the given file exists.
+	 * 
+	 * @param source
+	 */
+	public void assertFileExists(File source) {
+		assertFileExists("", source);
+	}
+	
+	/**
+	 * Assert that the given file exists.
+	 * 
+	 * @param source
+	 */
+	public void assertFileExists(String prefix, File source) {
+		assertTrue(prefix + "File '" + source.getAbsolutePath() + "' doesn't exist.", source.exists());
+	}
+	
+	/**
+	 * Read in a file into a string.
+	 * 
+	 * @throws IOException if an IO exception occurs
+	 */
+	public static String readFile(File sourceFile) throws IOException {
+		if (!sourceFile.exists()) {
+			throw new IOException("File " + sourceFile.getAbsolutePath() + " does not exist.");
+		}
+		
+		int bufSize = 128;
+		StringBuffer sb = new StringBuffer(bufSize);
+		BufferedReader reader = new BufferedReader(new FileReader(sourceFile), bufSize);
+				
+		char[] chars = new char[bufSize];
+		int numRead = 0;
+		while ((numRead = reader.read(chars)) > -1) {
+			sb.append(String.valueOf(chars).substring(0, numRead));	
+		}
+		
+		reader.close();
+		return sb.toString();
+	}
+
+	/**
+	 * Helper method: assert A >= B.
+	 * 
+	 * @param expected expected value (B)
+	 * @param actual actual value (A)
+	 */
+	protected void assertGreaterEq(int expected, int actual) {
+		assertTrue("expected >= than " + expected + ", but actually had " + actual, actual >= expected);
+	}
+	
 }

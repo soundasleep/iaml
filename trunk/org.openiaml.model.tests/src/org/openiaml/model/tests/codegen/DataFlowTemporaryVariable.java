6659112bdcbcb79ab740e4b78762027c61287b18
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2009-04-16 04:46:06 +0000

    adding DataFlowTemporaryVariable test case removing SessionConditions test case

---
+++ trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/codegen/DataFlowTemporaryVariable.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.openiaml.model.tests.codegen;
+
+import java.util.Date;
+
+import org.eclipse.core.resources.IFile;
+import org.openiaml.model.model.InternetApplication;
+import org.openiaml.model.tests.CodegenTestCase;
+
+/**
+ * Tests using temporary variables within operation modelling.
+ * 
+ * @author jmwright
+ *
+ */
+public class DataFlowTemporaryVariable extends CodegenTestCase {
+	
+	protected InternetApplication root;
+	
+	protected void setUp() throws Exception {
+		root = loadAndCodegen(ROOT + "codegen/DataFlowTemporaryVariable.iaml");
+	}
+	
+	public void testRequirement() throws Exception {
+		// go to sitemap
+		IFile sitemap = getProject().getFile("output/sitemap.html");
+		assertTrue("sitemap " + sitemap + " exists", sitemap.exists());
+		
+		// go to page
+		beginAtSitemapThenPage(sitemap, "container");
+		
+		String text1 = "initial text " + new Date().toString();
+		String text2 = "the second change " + new Date().toString();
+		{
+			String target = getLabelIDForText("target");
+			assertLabeledFieldEquals(target, "");
+			setLabeledFormElementField(target, text1);
+			
+			// this should be set in 'destination'
+			String destination = getLabelIDForText("destination");
+			assertLabeledFieldEquals(destination, text1);
+			
+			// if we set destination
+			setLabeledFormElementField(destination, text2);
+			
+			// it should not change the target
+			assertLabeledFieldEquals(target, text1);
+		}
+		
+	}
+
+}

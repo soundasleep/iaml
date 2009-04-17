201da90591acb7d26ee959c0f98c07bb2544f38f
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2009-04-17 04:38:21 +0000

    adding tests for database objects (both db and properties); they should synchronise immediately, not only on form submission having to add FileDomain* creation tools to the Visual editor (issue 36)

---
+++ trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/codegen/PropertiesFileWithInputForm.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package org.openiaml.model.tests.codegen;
+
+import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.openiaml.model.model.InternetApplication;
+import org.openiaml.model.tests.CodegenTestCase;
+
+/**
+ * Test properties files that are wrapped with inputforms (or not)
+ * 
+ * @author jmwright
+ *
+ */
+public class PropertiesFileWithInputForm extends CodegenTestCase {
+	
+	protected InternetApplication root;
+	
+	protected void setUp() throws Exception {
+		root = loadAndCodegen(ROOT + "codegen/DatabaseWithInputForm.iaml");
+	}
+	
+	public void testWithoutForm() throws Exception {
+		// go to sitemap
+		IFile sitemap = getProject().getFile("output/sitemap.html");
+		assertTrue("sitemap " + sitemap + " exists", sitemap.exists());
+		
+		// go to page
+		beginAtSitemapThenPage(sitemap, "without form");
+		
+		String text = "a changed value " + new Date().toString();
+		{
+			String target = getLabelIDForText("target");
+			assertLabeledFieldEquals(target, "");
+			setLabeledFormElementField(target, text);
+		}
+		
+		// reload page
+		gotoSitemapThenPage(sitemap, "without form");
+		{
+			// should have changed
+			String target = getLabelIDForText("target");
+			assertLabeledFieldEquals(target, text);
+		}
+		
+		// *restart* entire session
+		beginAtSitemapThenPage(sitemap, "without form");
+		{
+			// should be the same
+			String target = getLabelIDForText("target");
+			assertLabeledFieldEquals(target, text);
+		}
+		
+		// open up the properties file
+		Properties p = loadProperties("output/result.properties");
+		assertTrue(p.containsKey("value1"));
+		assertEquals(text, p.getProperty("value1"));		
+	}
+
+	public void testWithForm() throws Exception {
+		// go to sitemap
+		IFile sitemap = getProject().getFile("output/sitemap.html");
+		assertTrue("sitemap " + sitemap + " exists", sitemap.exists());
+		
+		// go to page
+		beginAtSitemapThenPage(sitemap, "with form");
+		
+		String text = "a changed value " + new Date().toString();
+		{
+			String target = getLabelIDForText("target");
+			assertLabeledFieldEquals(target, "");
+			setLabeledFormElementField(target, text);
+		}
+		
+		// reload page
+		gotoSitemapThenPage(sitemap, "with form");
+		{
+			// should have changed
+			String target = getLabelIDForText("target");
+			assertLabeledFieldEquals(target, text);
+		}
+		
+		// *restart* entire session
+		beginAtSitemapThenPage(sitemap, "with form");
+		{
+			// should be the same
+			String target = getLabelIDForText("target");
+			assertLabeledFieldEquals(target, text);
+		}		
+
+		// open up the properties file
+		Properties p = loadProperties("output/result.properties");
+		assertTrue(p.containsKey("value2"));
+		assertEquals(text, p.getProperty("value2"));		
+
+	}
+
+}

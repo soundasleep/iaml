edec5d6edab13409dc369b364034f5cb1826c656
Jevon Wright <soundasleep@users.noreply.github.com>
2009-05-18 07:04:19 +0000

    improving the output of the instrumentation; no longer refers to missing output lines instrumented code now refers to the correct line number (+1) replacing sample instrumentation

--- org.openiaml.model.codegen.oaw/src/org/openiaml/model/codegen/oaw/coverage/OutputCoveredCode.java
+++ org.openiaml.model.codegen.oaw/src/org/openiaml/model/codegen/oaw/coverage/OutputCoveredCode.java
@@ -22,8 +22,8 @@ public class OutputCoveredCode {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws InstrumentationException, IOException {
-		File oawDump = new File("instrument/instrumented-1242624266481.dump");
-		File phpDump = new File("instrument/php-instrument-1242624276481.dump");
+		File oawDump = new File("instrument/instrumented-1242629587096.dump");
+		File phpDump = new File("instrument/php-instrument-1242629595393.dump");
 		File templatesDir = new File("src/template");
 		File outputDir = new File("instrument/output");
 

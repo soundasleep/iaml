034c5d097ca9af7a3b238a8c56066488e3d69bb6
Jevon Wright <soundasleep@users.noreply.github.com>
2009-05-19 01:37:32 +0000

    multiple test cases can now be instrumented to the same instrumentation files

--- org.openiaml.model.codegen.oaw/src/org/openiaml/model/codegen/oaw/coverage/RunInstrumentation.java
+++ org.openiaml.model.codegen.oaw/src/org/openiaml/model/codegen/oaw/coverage/RunInstrumentation.java
@@ -27,6 +27,21 @@ public class RunInstrumentation {
 		} catch (InstrumentationException e) {
 			e.printStackTrace();
 		}
+		
+		// we also need to delete the instrumentation files that
+		// already exist
+		for (File f : new File[]{ 
+				new File("instrument/instrumented.dump"),
+				new File("instrument/php-instrumented.dump"),
+				new File("instrument/php-instrumented.dump.raw")}) {
+			if (f.exists()) {
+				System.out.println("Deleting '" + f + "'...");
+				boolean deleted = f.delete();
+				if (!deleted) {
+					throw new RuntimeException("Could not delete '" + f + "'");
+				}
+			}
+		}
 
 		System.out.println("Instrumentation applied.");
 	}

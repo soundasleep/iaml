84ee8f6d43e25b165c0132c29e3966bd766878b6
Jevon Wright <soundasleep@users.noreply.github.com>
2009-07-13 03:12:48 +0000

    merging successful results of issue 47 branch into trunk

--- org.openiaml.model.tests/src/org/openiaml/model/tests/eclipse/shortcuts/EclipseShortcutsTestSuite.java
+++ org.openiaml.model.tests/src/org/openiaml/model/tests/eclipse/shortcuts/EclipseShortcutsTestSuite.java
@@ -26,6 +26,7 @@ public class EclipseShortcutsTestSuite {
         suite.addTestSuite(ShortcutsOperationTestCase.class);
         suite.addTestSuite(ShortcutsRootTestCase.class);
         suite.addTestSuite(ShortcutsWireTestCase.class);
+        suite.addTestSuite(Issue47.class);
 
         return suite; 
    }

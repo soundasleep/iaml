cd651593dad48d029350ce5ee50d9d9093c59190
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2009-08-07 10:00:02 +0000

    issue 63: adding an action to remove phantom edges in the model adding test case to test its runtime effects moving all iaml tool actions into a submenu moving InfiniteSubProgressMonitor into a global class

---
+++ trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/eclipse/actions/RemovePhantomEdges.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package org.openiaml.model.tests.eclipse.actions;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.openiaml.model.diagram.custom.actions.RemovePhantomEdgesAction;
+import org.openiaml.model.model.InternetApplication;
+import org.openiaml.model.model.WireEdge;
+import org.openiaml.model.model.visual.Page;
+import org.openiaml.model.tests.EclipseTestCaseHelper;
+
+/**
+ * Issue 63: Check the tool to remove phantom edges from our model.
+ * 
+ * @author jmwright
+ *
+ */
+public class RemovePhantomEdges extends EclipseTestCaseHelper {
+	
+	private IFile targetModel;
+	
+	public void copyFiles() throws Exception {
+		// register errors
+		addLogListener();
+
+		// copy our local file into the project
+		targetModel = project.getFile("RemovePhantomEdges.iaml");
+		copyFileIntoWorkspace("src/org/openiaml/model/tests/eclipse/actions/RemovePhantomEdges.iaml",
+				targetModel);
+		
+		assertExists(targetModel);
+	}
+	
+	/**
+	 * Test the initial model - it should contain phantom edges.
+	 * 
+	 * @throws Exception
+	 */
+	public void testInitial() throws Exception {
+		copyFiles();
+		
+		// try and load the file directly
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource resource = resourceSet.getResource(URI.createPlatformResourceURI(targetModel.getFullPath().toString(), false), true);		
+		InternetApplication model = (InternetApplication) resource.getContents().get(0);
+		
+		// get the Page
+		assertEquals(1, model.getChildren().size());
+		Page page = (Page) model.getChildren().get(0);
+		
+		assertEquals(3, page.getWires().size());
+		
+		for (WireEdge w : model.getWires()) {
+			assertNotNull(w.getFrom());
+			assertNull(w.getTo());
+		}
+	}
+
+	/**
+	 * Test the actioned model - it should no longer contain phantom edges.
+	 * 
+	 * @throws Exception
+	 */
+	public void testRemovePhantomEdges() throws Exception {
+		// copy files
+		copyFiles();
+		
+		// do the phantom edges action
+		RemovePhantomEdgesAction act = new RemovePhantomEdgesAction();
+		act.doExecute(targetModel, new NullProgressMonitor());
+
+		// try and load the file directly
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource resource = resourceSet.getResource(URI.createPlatformResourceURI(targetModel.getFullPath().toString(), false), true);		
+		InternetApplication model = (InternetApplication) resource.getContents().get(0);
+		
+		// get the Page
+		assertEquals(1, model.getChildren().size());
+		Page page = (Page) model.getChildren().get(0);
+
+		// no more wires
+		assertEquals(0, page.getWires().size());
+
+	}
+	
+}

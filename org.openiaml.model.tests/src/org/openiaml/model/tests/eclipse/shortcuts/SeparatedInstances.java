5f7d2530f3fc41846fe35eb5e48885059544155e
Jevon Wright <soundasleep@users.noreply.github.com>
2009-08-19 07:28:05 +0000

    operation diagrams can be created independently of root diagrams adding a test case (which doesn't yet pass) for loading these separated instances

---
+++ org.openiaml.model.tests/src/org/openiaml/model/tests/eclipse/shortcuts/SeparatedInstances.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package org.openiaml.model.tests.eclipse.shortcuts;
+
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeNodeEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor;
+
+/**
+ * We can separate model instances into multiple files, which all
+ * refer to each other. However we can't yet this this programatically,
+ * as a NPE is thrown. Perhaps an Eclipse application is loading the
+ * separate model in a different way?
+ * 
+ * @see #getModel()
+ * @author jmwright
+ *
+ */
+@Deprecated
+public class SeparatedInstances extends AbstractShortcutsTestCase {
+
+	public String getModel() {
+		return "SeparatedInstances.iaml";
+	}
+	
+	protected DiagramDocumentEditor editor_page;
+	
+	public void testEclipseShortcuts() throws Exception {
+		initializeModelFile();
+		assertEditorRoot(editor);
+
+		assertEditorHasChildren(2, editor);
+		
+		ShapeNodeEditPart page = assertHasPage(editor, "Home", false);
+		assertHasOperation(editor, "root operation", false);
+		
+		// open the page
+		editor_page = openDiagram(page);
+		assertEditorVisual(editor_page);
+
+		ShapeNodeEditPart event = assertHasEventTrigger(editor_page, "access", false);
+		// shortcut
+		ShapeNodeEditPart op = assertHasOperation(editor_page, "target operation", true);
+		assertHasRunInstanceWire(editor_page, event, op, "run");
+		
+		assertEditorHasChildren(2, editor_page);
+
+	}
+
+	/**
+	 * Close loaded editors.
+	 * @throws Exception 
+	 */
+	public void tearDown() throws Exception {
+		
+		if (editor_page != null)
+			editor_page.close(false);
+		
+		super.tearDown();
+	}
+
+}

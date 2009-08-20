ebb4e884932b84846278576ed99b2e2747dac52e
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2009-08-20 14:05:47 +0000

    refreshing gmfgen diffs, except for operation.gmfgen.diff diffs now provide 5 lines of context, rather than 3, to hopefully reduce match fuzz errors

---
+++ trunk/org.openiaml.model/model/scripts/apply-all-diffs.bat
@@ -0,0 +1,10 @@
+rem apply condition
+apply domain_object_instance
+apply domain_object
+apply domain_store
+apply element
+apply operation
+apply root
+rem apply user_store
+apply visual
+apply wire

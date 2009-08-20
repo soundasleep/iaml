e2cefa3cdff0754cd7f010977af7357c13a228bb
Jevon Wright <soundasleep@users.noreply.github.com>
2009-08-20 14:05:47 +0000

    refreshing gmfgen diffs, except for operation.gmfgen.diff diffs now provide 5 lines of context, rather than 3, to hopefully reduce match fuzz errors

--- org.openiaml.model/model/scripts/make-diff.bat
+++ org.openiaml.model/model/scripts/make-diff.bat
@@ -2,6 +2,6 @@
 rem -- create diff's for %1 model .gmfgen --
 
 cd ..
-mkdir diff
-diff -u -w %1.gmfgen copies/%1.gmfgen > diff/%1.gmfgen.diff
+rem mkdir diff
+diff -U 5 -w %1.gmfgen copies/%1.gmfgen > diff/%1.gmfgen.diff
 cd scripts

36aa0d848206b06fbe000cc8024434bf5507fddf
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2009-09-10 11:29:13 +0000

    thanks to r1138, the diff and scripts components of the org.openiaml.model are no longer necessary

--- trunk/org.openiaml.model/model/scripts/make-diff.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem -- create diff's for %1 model .gmfgen --
-
-cd ..
-rem mkdir diff
-diff -U 3 -w %1.gmfgen copies/%1.gmfgen > diff/%1.gmfgen.diff
-svn revert %1.gmfgen
-cd scripts
-@echo on

872ec4032ed69dd388c0ff922d3c0aa761a9f351
Jevon Wright <soundasleep@users.noreply.github.com>
2009-09-10 11:29:13 +0000

    thanks to r1138, the diff and scripts components of the org.openiaml.model are no longer necessary

--- org.openiaml.model/model/scripts/apply.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem -- apply the diff for %1 model .gmfgen --
-
-cd ..
-patch %1.gmfgen diff/%1.gmfgen.diff
-if exist *.orig rm *.orig
-cd scripts
-@echo on
--- org.openiaml.model/model/scripts/update-copies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem -- updates the copies directory with the latest from SVN --
-rem -- assumes that cygwin is installed --
-
-cd ..
-cd copies
-svn update .
-cd ..
-cd scripts

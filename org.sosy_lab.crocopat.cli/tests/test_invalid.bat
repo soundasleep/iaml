ba1de5b254da8823805f6924fddd9d35bde88a70
Jevon Wright <soundasleep@users.noreply.github.com>
2009-10-28 08:26:48 +0000

    moving tests to separate plugin

--- org.sosy_lab.crocopat.cli/tests/test_invalid.bat
+++
@@ -1,4 +0,0 @@
-"..\crocopat\crocopat-2.1.4_win32.exe" test.rml < test.rsf
-"..\crocopat\crocopat-2.1.4_win32.exe" test.rml < invalid.rsf
-"..\crocopat\crocopat-2.1.4_win32.exe" invalid.rml < test.rsf
-"..\crocopat\crocopat-2.1.4_win32.exe" invalid.rml < invalid.rsf

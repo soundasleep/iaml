94f79ded8cb906a7b89d24e33cd3b4bfe2dd1ab3
Jevon Wright <soundasleep@users.noreply.github.com>
2010-01-13 06:22:41 +0000

    refactoring out old initializeModelFile from eclipse test case

--- org.openiaml.model.tests.diagram/src/org/openiaml/model/tests/eclipse/EclipseTestCase.java
+++ org.openiaml.model.tests.diagram/src/org/openiaml/model/tests/eclipse/EclipseTestCase.java
@@ -3,42 +3,24 @@
  */
 package org.openiaml.model.tests.eclipse;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.operations.OperationHistoryFactory;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.RequestConstants;
 import org.eclipse.gef.requests.SelectionRequest;
-import org.eclipse.gmf.runtime.common.core.command.CommandResult;
-import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
 import org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor;
-import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
-import org.eclipse.gmf.runtime.emf.core.GMFEditingDomainFactory;
-import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
-import org.openiaml.model.diagram.edit.parts.InternetApplicationEditPart;
-import org.openiaml.model.diagram.part.IamlDiagramEditorPlugin;
+import org.openiaml.model.diagram.custom.helpers.DiagramRegistry;
 import org.openiaml.model.diagram.part.IamlDiagramEditorUtil;
 import org.openiaml.model.diagram.part.IamlInitDiagramFileAction;
 import org.openiaml.model.diagram.part.IamlNewDiagramFileWizard;
-import org.openiaml.model.diagram.part.IamlVisualIDRegistry;
-import org.openiaml.model.diagram.part.Messages;
 import org.openiaml.model.tests.ModelInferenceTestCase;
 import org.openiaml.model.tests.ModelTestCase;
 
@@ -57,6 +39,7 @@ public abstract class EclipseTestCase extends ModelTestCase {
 	 * @throws Exception 
 	 * @see {@link #createProject()} 
 	 */
+	@Override
 	public void setUp() throws Exception {
 		// create the project
 		project = createProject();
@@ -65,6 +48,7 @@ public abstract class EclipseTestCase extends ModelTestCase {
 	/**
 	 * When we close the test case, we should also close the project.
 	 */
+	@Override
 	public void tearDown() throws Exception {
 		project.close(monitor);
 	}
@@ -115,51 +99,10 @@ public abstract class EclipseTestCase extends ModelTestCase {
 	 * @param diagramFile must not exist yet
 	 */
 	protected void initializeModelFile(IFile modelFile, IFile diagramFile) throws Exception {
-		assertTrue("model file exists", modelFile.exists());
-		assertFalse("diagram file doesn't exist", diagramFile.exists());
-
-		// initialize model file
-		// based on generated IamlNewDiagramFileWizard#performFinish
-		// based on generated IamlInitDiagramFileAction#run
-		ResourceSet resourceSet = new ResourceSetImpl();
-		URI sourceModelURI = URI.createPlatformResourceURI(modelFile.getFullPath().toString(), false);
-		URI diagramModelURI = URI.createPlatformResourceURI(diagramFile.getFullPath().toString(), false);
-		final Resource modelResource = resourceSet.getResource(sourceModelURI, true);
-		final Resource diagramResource = resourceSet.createResource(diagramModelURI);
-		final EObject modelRoot = (EObject) modelResource.getContents().get(0);
 		
-		List<Object> affectedFiles = new LinkedList<Object>();
-		affectedFiles.add(diagramFile);
-		TransactionalEditingDomain myEditingDomain = GMFEditingDomainFactory.INSTANCE
-			.createEditingDomain();
+		// we now ask the DiagramRegistry to initialise the diagram for us (and open it)
+		DiagramRegistry.initializeModelFile(modelFile, diagramFile);
 		
-		AbstractTransactionalCommand command = new AbstractTransactionalCommand(
-				myEditingDomain,
-				Messages.IamlNewDiagramFileWizard_InitDiagramCommand,
-				affectedFiles) {
-
-			protected CommandResult doExecuteWithResult(
-					IProgressMonitor monitor, IAdaptable info)
-					throws ExecutionException {
-				int diagramVID = IamlVisualIDRegistry
-						.getDiagramVisualID(modelRoot);
-				if (diagramVID != InternetApplicationEditPart.VISUAL_ID) {
-					return CommandResult
-							.newErrorCommandResult(Messages.IamlNewDiagramFileWizard_IncorrectRootError);
-				}
-				Diagram diagram = ViewService.createDiagram(
-						modelRoot,
-						InternetApplicationEditPart.MODEL_ID,
-						IamlDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);
-				diagramResource.getContents().add(diagram);
-				return CommandResult.newOKCommandResult();
-			}
-		};
-		OperationHistoryFactory.getOperationHistory().execute(command,
-				new NullProgressMonitor(), null);
-		diagramResource.save(IamlDiagramEditorUtil.getSaveOptions());
-		IamlDiagramEditorUtil.openDiagram(diagramResource);
-		// end
 	}
 
 	/**

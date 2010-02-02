18fa0c8a94ec66254482b7c773b3437e4ca0b01a
Jevon Wright <soundasleep@users.noreply.github.com>
2010-02-02 07:16:09 +0000

    adding inference and codegen tests for the GateRequiredPage test model

--- org.openiaml.model.tests/src/org/openiaml/model/tests/inference/model0_4_2/AllModel0_4_2InferenceTests.java
+++ org.openiaml.model.tests/src/org/openiaml/model/tests/inference/model0_4_2/AllModel0_4_2InferenceTests.java
@@ -1,16 +1,15 @@
-package org.openiaml.model.tests.codegen.model0_4_2;
+package org.openiaml.model.tests.inference.model0_4_2;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-
 /**
- * Code generation tests: model version 0.4.1
+ * All inference tests for model 0.4.2.
  * 
  * @author jmwright
  *
  */
-public class AllModel0_4_2CodegenTests {
+public class AllModel0_4_2InferenceTests {
 
 	/**
 	 * Get all the tests in this package and return as a test suite.
@@ -20,10 +19,9 @@ public class AllModel0_4_2CodegenTests {
 	public static Test suite() { 
 		TestSuite suite = new TestSuite("Model 0.4.2");
 		
-		suite.addTestSuite(GateDisclaimer.class);
 		suite.addTestSuite(GateRequiredPage.class);
-
+		
 		return suite;
 	}
-
+	
 }

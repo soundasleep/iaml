c2f4c83e206a4324e42b3ed9ceda99bc779c5a5c
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2010-03-17 05:34:13 +0000

    adding test case for InputTextFieldDataTypeSyncChained setPropertyToValue no longer mangles differently typed elements (i.e. dates) from implicit toString() calls

---
+++ trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/codegen/model0_4_4/InputTextFieldDataTypeSyncChained.java
@@ -0,0 +1,220 @@
+/**
+ * 
+ */
+package org.openiaml.model.tests.codegen.model0_4_4;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import org.openiaml.model.tests.CodegenTestCase;
+
+/**
+ * Chained SyncWires.
+ */
+public class InputTextFieldDataTypeSyncChained extends CodegenTestCase {
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		root = loadAndCodegen(InputTextFieldDataTypeSyncChained.class);
+	}
+	
+	/**
+	 * Override to check also for warnings.
+	 */
+	@Override
+	protected void assertProblem() {
+		resetDebug();
+		assertMatch("(Error|error|Exception|exception|Warning|warning)");
+	}
+
+	/**
+	 * Override to check also for warnings.
+	 */
+	@Override
+	protected void assertNoProblem() {
+		resetDebug();
+		assertNoMatch("(Error|error|Exception|exception|Warning|warning)");
+	}
+	
+	/**
+	 * The home page can be accessed.
+	 * 
+	 * @throws Exception 
+	 */
+	public void testHome() throws Exception {
+		
+		beginAtSitemapThenPage("Home");
+		assertNoProblem();
+				
+	}
+	
+	public void testSetString() throws Exception {
+		
+		beginAtSitemapThenPage("Home");
+		assertNoProblem();	// no problem to begin with
+		
+		String text = "hello, world!";
+		{
+			String target = getLabelIDForText("String");
+			assertLabeledFieldEquals(target, "");	// empty
+			
+			// we can set it to a string
+			setLabeledFormElementField(target, text);
+			assertLabeledFieldEquals(target, text);
+		}
+		
+		// 'Default' is changed
+		assertLabel("Default", text);
+		
+		// Integer is not
+		assertLabel("Integer", "");
+		
+		// Email is not
+		assertLabel("Email", "");
+		
+		// Date and Date 2 are not
+		assertLabel("Date", "");
+		assertLabel("Date 2", "");
+		
+		// in fact, there is a warning because of the lack of conversion
+		assertProblem();
+		
+		// an unrelated field, String 2, is unchanged
+		assertLabel("String 2", "");
+		
+	}
+	
+	/**
+	 * If we set 'String' to a valid date, it will pass along
+	 * 
+	 * @throws Exception
+	 */
+	public void testSetStringDate() throws Exception {
+		
+		beginAtSitemapThenPage("Home");
+		assertNoProblem();	// no problem to begin with
+		
+		String text = "25 dec 1998 +0000";
+		
+		// conversion
+		Calendar c = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		c.clear();
+		c.set(1998, 11, 25);
+		String expected = rfc2822(c.getTime());
+		
+		{
+			String target = getLabelIDForText("String");
+			assertLabeledFieldEquals(target, "");	// empty
+			
+			// we can set it to a string
+			setLabeledFormElementField(target, text);
+			assertLabeledFieldEquals(target, expected);	// changed
+		}
+		
+		// 'Default' is changed
+		assertLabel("Default", expected);
+		
+		// Date and Date 2 are
+		assertLabel("Date", expected);
+		assertLabel("Date 2", expected);
+		
+		// Integer is not
+		assertLabel("Integer", "");
+		
+		// Email is not
+		assertLabel("Email", "");
+
+		// in fact, there is a warning because of the lack of conversion
+		assertProblem();
+		
+		// an unrelated field, String 2, is unchanged
+		assertLabel("String 2", "");
+		
+	}
+	
+	/**
+	 * If we set 'Integer' to a valid integer, it will pass along
+	 * 
+	 * @throws Exception
+	 */
+	public void testSetInteger() throws Exception {
+		
+		beginAtSitemapThenPage("Home");
+		assertNoProblem();	// no problem to begin with
+		
+		String text = "1642015";
+		
+		{
+			String target = getLabelIDForText("Integer");
+			assertLabeledFieldEquals(target, "");	// empty
+			
+			// we can set it to a string
+			setLabeledFormElementField(target, text);
+			assertLabeledFieldEquals(target, text);
+		}
+		
+		// 'Default' is changed
+		assertLabel("Default", text);
+
+		// 'String' is changed
+		assertLabel("String", text);
+
+		// Date and Date 2 are not
+		assertLabel("Date", "");
+		assertLabel("Date 2", "");
+		
+		// Email is not
+		assertLabel("Email", "");
+
+		// in fact, there is a warning because of the lack of conversion
+		assertProblem();
+		
+		// an unrelated field, String 2, is unchanged
+		assertLabel("String 2", "");
+		
+	}
+	
+	/**
+	 * If we set 'Integer' to an invalid integer, nothing will happen.
+	 * 
+	 * @throws Exception
+	 */
+	public void testInvalidInteger() throws Exception {
+		
+		beginAtSitemapThenPage("Home");
+		assertNoProblem();	// no problem to begin with
+		
+		String text = "hello, world!";
+		
+		{
+			String target = getLabelIDForText("Integer");
+			assertLabeledFieldEquals(target, "");	// empty
+			
+			setLabeledFormElementField(target, text);
+			
+			// it does not change
+			assertLabeledFieldEquals(target, "");
+		}
+		
+		// none of the chained elements change
+		assertLabel("Default", "");
+		assertLabel("String", "");
+		assertLabel("Date", "");
+		assertLabel("Date 2", "");
+		assertLabel("Email", "");
+		
+		// there is no problem, because the non-integer wasn't even accepted
+		assertNoProblem();
+		
+		// an unrelated field, String 2, is unchanged
+		assertLabel("String 2", "");
+		
+	}
+	
+	private void assertLabel(String labelName, String text) {
+		String target = getLabelIDForText(labelName);
+		assertLabeledFieldEquals(target, text);
+	}
+
+}

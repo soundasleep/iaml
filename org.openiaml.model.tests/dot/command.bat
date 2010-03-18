d068dbeebaa08bfb2cff9b19994490174feac642
Jevon Wright <soundasleep@users.noreply.github.com>
2010-03-18 23:26:55 +0000

    updating dot graphs

--- org.openiaml.model.tests/dot/command.bat
+++ org.openiaml.model.tests/dot/command.bat
@@ -1,4 +1,5 @@
 @rem Use one of: circo dot fdp neato nop nop1 nop2 osage patchwork sfdp twopi
 dot all-dependencies.dot.txt -O -Kneato -Tpng && start all-dependencies.dot.txt.png
 dot local-dependencies.dot.txt -O -Kdot -Tpng && start local-dependencies.dot.txt.png
-dot local-dependencies-notests.dot.txt -O -Ksfdp -Tpng && start local-dependencies-notests.dot.txt.png
+dot local-dependencies-notests.dot.txt -O -Kdot -Tpng && start local-dependencies-notests.dot.txt.png
+dot local-dependencies-notests-onediagram.dot.txt -O -Kdot -Tpng && start local-dependencies-notests-onediagram.dot.txt.png

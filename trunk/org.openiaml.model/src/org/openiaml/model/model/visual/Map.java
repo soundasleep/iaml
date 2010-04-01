5877b1d45976e7d9bb1525f0933b7c6d8adf8ea9
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2010-04-01 07:07:22 +0000

    adding Map and MapPoint to metamodel

---
+++ trunk/org.openiaml.model/src/org/openiaml/model/model/visual/Map.java
@@ -0,0 +1,26 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.openiaml.model.model.visual;
+
+import org.openiaml.model.model.VisibleThing;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Map</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <!-- begin-model-doc -->
+ * Represents a mapping component, that can be interacted with. This may contain {@model MapPoint}s which are also rendered on the {@model Map}.
+ * <!-- end-model-doc -->
+ *
+ *
+ * @see org.openiaml.model.model.visual.VisualPackage#getMap()
+ * @model
+ * @generated
+ */
+public interface Map extends VisibleThing {
+} // Map

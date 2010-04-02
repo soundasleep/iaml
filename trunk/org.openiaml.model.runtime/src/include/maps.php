d6017096bb3dd656a393d2023d5d52c31c2daa3a
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2010-04-02 04:38:03 +0000

    fixing implementation of mock map handler so test case passes

--- trunk/org.openiaml.model.runtime/src/include/maps.php
+++ trunk/org.openiaml.model.runtime/src/include/maps.php
@@ -33,22 +33,37 @@ function echo_mock_map_point($id) {
 	var hidden_point_<?php echo $id; ?> = null;
 
 	function show_map_point_<?php echo $id; ?>() {
-		document.getElementById('<?php echo $id; ?>').style.display = "";
+		if (hidden_label_<?php echo $id; ?> != null) {
+			document.getElementById('<?php echo $id; ?>_label').innerHTML = hidden_label_<?php echo $id; ?>;
+			document.getElementById('mock_map_point_<?php echo $id; ?>_address').innerHTML = hidden_point_<?php echo $id; ?>;
+			hidden_label_<?php echo $id; ?> = null;
+			hidden_point_<?php echo $id; ?> = null;
+		}
 		document.getElementById('<?php echo $id; ?>_label').style.display = "";
+		document.getElementById('mock_map_point_<?php echo $id; ?>_address').style.display = "";
 	}
 	function hide_map_point_<?php echo $id; ?>() {
-		document.getElementById('<?php echo $id; ?>').style.display = "none";
+		hidden_label_<?php echo $id; ?> = document.getElementById('<?php echo $id; ?>_label').innerHTML;
+		hidden_point_<?php echo $id; ?> = document.getElementById('mock_map_point_<?php echo $id; ?>_address').innerHTML;
+		document.getElementById('<?php echo $id; ?>_label').innerHTML = "";
+		document.getElementById('mock_map_point_<?php echo $id; ?>_address').innerHTML = "";
 		document.getElementById('<?php echo $id; ?>_label').style.display = "none";
+		document.getElementById('mock_map_point_<?php echo $id; ?>_address').style.display = "none";
 	}
 	function set_map_point_<?php echo $id; ?>(address) {
 		if (address) {
+			// show it
 			show_map_point_<?php echo $id; ?>();
+
+			document.getElementById("mock_map_point_<?php echo $id; ?>_address")
+				.innerHTML = address;
 		} else {
+			// hide it
+			document.getElementById("mock_map_point_<?php echo $id; ?>_address")
+				.innerHTML = address;
+
 			hide_map_point_<?php echo $id; ?>();
 		}
-	
-		document.getElementById("mock_map_point_<?php echo $id; ?>_address")
-			.innerHTML = address;
 	}
 	
 	// and register this to the handler, for callbacks to update the map point

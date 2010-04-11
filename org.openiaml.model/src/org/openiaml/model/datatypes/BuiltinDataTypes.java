103733b9695edff0887a5ebbe0782f359476cc53
Jevon Wright <soundasleep@users.noreply.github.com>
2010-04-11 08:21:05 +0000

    adding support for loading XSDSimpleTypeDefinitions directly, by loading the XSD model using the plugin adding test case to check that this loading is correct

--- org.openiaml.model/src/org/openiaml/model/datatypes/BuiltinDataTypes.java
+++ org.openiaml.model/src/org/openiaml/model/datatypes/BuiltinDataTypes.java
@@ -3,6 +3,14 @@
  */
 package org.openiaml.model.datatypes;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xsd.XSDSchema;
+import org.eclipse.xsd.XSDSimpleTypeDefinition;
+import org.eclipse.xsd.XSDTypeDefinition;
+import org.openiaml.model.ModelLoader;
+import org.openiaml.model.ModelLoader.ModelLoadException;
+
 /**
  * A convenience list of constants representing the different 
  * built-in data types. 
@@ -13,7 +21,11 @@ package org.openiaml.model.datatypes;
  * @author jmwright
  *
  */
-public interface BuiltinDataTypes {
+public class BuiltinDataTypes {
+	
+	private BuiltinDataTypes() {
+		// not meant to be instantiated
+	}
 	
 	/**
 	 * String data type.
@@ -34,5 +46,132 @@ public interface BuiltinDataTypes {
 	 * Integer data type.
 	 */
 	public static final String TYPE_INTEGER = "http://openiaml.org/model/datatypes#iamlInteger";
+	
+	private static XSDSimpleTypeDefinition xsdString = null;
+	private static XSDSimpleTypeDefinition xsdEmail = null;
+	private static XSDSimpleTypeDefinition xsdDateTime = null;
+	private static XSDSimpleTypeDefinition xsdInteger = null;
+	
+	/**
+	 * Get the {@link XSDSimpleTypeDefinition} for {@link #TYPE_INTEGER}.
+	 * May call {@link #updateTypeDefinitions()} if the types have not been
+	 * loaded from the plugin yet.
+	 *  
+	 * @return the XSDSimpleTypeDefinition for TYPE_INTEGER
+	 */
+	public static final XSDSimpleTypeDefinition getTypeInteger() {
+		if (xsdInteger == null) {
+			updateTypeDefinitions();
+		}
+		return xsdInteger;
+	}
+	
+	/**
+	 * Get the {@link XSDSimpleTypeDefinition} for {@link #TYPE_DATETIME}.
+	 * May call {@link #updateTypeDefinitions()} if the types have not been
+	 * loaded from the plugin yet.
+	 *  
+	 * @return the XSDSimpleTypeDefinition for TYPE_DATETIME
+	 */
+	public static final XSDSimpleTypeDefinition getTypeDateTime() {
+		if (xsdDateTime == null) {
+			updateTypeDefinitions();
+		}
+		return xsdDateTime;
+	}
+	
+	/**
+	 * Get the {@link XSDSimpleTypeDefinition} for {@link #TYPE_EMAIL}.
+	 * May call {@link #updateTypeDefinitions()} if the types have not been
+	 * loaded from the plugin yet.
+	 *  
+	 * @return the XSDSimpleTypeDefinition for TYPE_EMAIL
+	 */
+	public static final XSDSimpleTypeDefinition getTypeEmail() {
+		if (xsdEmail == null) {
+			updateTypeDefinitions();
+		}
+		return xsdEmail;
+	}
+	
+	/**
+	 * Get the {@link XSDSimpleTypeDefinition} for {@link #TYPE_STRING}.
+	 * May call {@link #updateTypeDefinitions()} if the types have not been
+	 * loaded from the plugin yet.
+	 *  
+	 * @return the XSDSimpleTypeDefinition for TYPE_STRING
+	 */
+	public static final XSDSimpleTypeDefinition getTypeString() {
+		if (xsdString == null) {
+			updateTypeDefinitions();
+		}
+		return xsdString;
+	}
+	
+	/**
+	 * The source of the builtin data types.
+	 */
+	public static final String TYPE_DEFINITIONS = "platform:/plugin/org.openiaml.model/model/datatypes.xsd"; 
+	
+	/**
+	 * Load the XSD file at {@link #TYPE_DEFINITIONS} and populate the
+	 * related XSD definitions, such as {@link #getTypeInteger()}.
+	 * 
+	 * @throws BuiltinTypeInitializationException if an exception occurs
+	 */
+	protected static void updateTypeDefinitions() {
+		URI uri = URI.createURI(TYPE_DEFINITIONS);
+		if (uri == null) {
+			throw new BuiltinTypeInitializationException("URI for '" + TYPE_DEFINITIONS + "' was null");
+		}
+		EObject root;
+		try {
+			root = ModelLoader.load(uri);
+		} catch (ModelLoadException e) {
+			throw new BuiltinTypeInitializationException("Could not load URI " + uri, e);
+		}
+		
+		XSDSchema schema = (XSDSchema) root;
+		for (XSDTypeDefinition type : schema.getTypeDefinitions()) {
+			if (type instanceof XSDSimpleTypeDefinition) {
+				XSDSimpleTypeDefinition stype = (XSDSimpleTypeDefinition) type;
+				String u = stype.getURI();
+				
+				// for every type
+				if (TYPE_INTEGER.equals(u)) {
+					xsdInteger = stype;
+				} else if (TYPE_STRING.equals(u)) {
+					xsdString = stype;
+				} else if (TYPE_EMAIL.equals(u)) {
+					xsdEmail = stype;
+				} else if (TYPE_DATETIME.equals(u)) {
+					xsdDateTime = stype;
+				}
+			}
+		}
+	
+	}
+	
+	/**
+	 * A runtime exception indicating something bad happened while
+	 * trying to initialise the built-in XSD types.
+	 * 
+	 * @author jmwright
+	 */
+	public static class BuiltinTypeInitializationException extends RuntimeException {
+
+		private static final long serialVersionUID = 1L;
+		
+		public BuiltinTypeInitializationException(String message) {
+			super(message);
+		}
+		public BuiltinTypeInitializationException(Throwable cause) {
+			super(cause.getMessage(), cause);
+		}
+		public BuiltinTypeInitializationException(String message, Throwable cause) {
+			super(message, cause);
+		}
+		
+	}
 	
 }

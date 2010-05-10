d81977269f01d0efa35c157a2b993717c98ee815
Jevon Wright <soundasleep@users.noreply.github.com>
2010-05-10 04:47:54 +0000

    more work on temporary migration script: adding SelectWire and NewInstanceWire, and resolving most unresolved references

--- org.openiaml.model.actions/src/org/openiaml/model/custom/actions/MigrateAllModelsAction2.java
+++ org.openiaml.model.actions/src/org/openiaml/model/custom/actions/MigrateAllModelsAction2.java
@@ -18,13 +18,15 @@ import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.openiaml.model.ModelLoader;
-import org.openiaml.model.ModelLoader.ModelLoadException;
+import org.openiaml.model.model.ContainsWires;
+import org.openiaml.model.model.DomainAttributeInstance;
 import org.openiaml.model.model.DomainObject;
 import org.openiaml.model.model.DomainObjectInstance;
 import org.openiaml.model.model.DomainStore;
 import org.openiaml.model.model.InternetApplication;
 import org.openiaml.model.model.ModelPackage;
 import org.openiaml.model.model.Scope;
+import org.openiaml.model.model.Wire;
 import org.openiaml.model.model.domain.DomainFactory;
 import org.openiaml.model.model.domain.DomainIterator;
 import org.openiaml.model.model.domain.DomainPackage;
@@ -33,7 +35,11 @@ import org.openiaml.model.model.domain.DomainSource;
 import org.openiaml.model.model.domain.DomainStoreTypes;
 import org.openiaml.model.model.domain.RemoteDomainObject;
 import org.openiaml.model.model.domain.SchemaEdge;
-import org.openiaml.model.model.wires.WiresFactory;
+import org.openiaml.model.model.domain.SelectEdge;
+import org.openiaml.model.model.users.UserStore;
+import org.openiaml.model.model.wires.NewInstanceWire;
+import org.openiaml.model.model.wires.SelectWire;
+import org.openiaml.model.model.wires.WiresPackage;
 
 /**
  * A temporary migrator that uses EMF.
@@ -87,6 +93,11 @@ public class MigrateAllModelsAction2 extends ProgressEnabledAction<IContainer> {
 								DomainIterator di = DomainFactory.eINSTANCE.createDomainIterator();
 								
 								for (EAttribute attr : doi.eClass().getEAllAttributes()) {
+									if (attr.equals(ModelPackage.eINSTANCE.getGeneratedElement_Id())) {
+										// ignore 'id'
+										continue;
+									}
+
 									di.eSet(attr, doi.eGet(attr));
 								}
 								
@@ -144,7 +155,12 @@ public class MigrateAllModelsAction2 extends ProgressEnabledAction<IContainer> {
 
 								ds.setType(frame.getType());
 								ds.setFile(frame.getFile());
-
+								
+								// copy over DomainStore.permissions directly
+								if (frame instanceof UserStore) {
+									di.getPermissions().addAll(((UserStore) frame).getPermissions());
+								}
+								
 								// set the type
 								if (doi instanceof RemoteDomainObject) {
 									RemoteDomainObject rdo = (RemoteDomainObject) doi;
@@ -154,6 +170,11 @@ public class MigrateAllModelsAction2 extends ProgressEnabledAction<IContainer> {
 								}
 
 								for (EAttribute attr : doi.eClass().getEAllAttributes()) {
+									if (attr.equals(ModelPackage.eINSTANCE.getGeneratedElement_Id())) {
+										// ignore 'id'
+										continue;
+									}
+
 									if (attr.equals(ModelPackage.eINSTANCE.getChangeable_DefaultValue())) {
 										// drop 'defaultValue'
 										continue;
@@ -215,7 +236,17 @@ public class MigrateAllModelsAction2 extends ProgressEnabledAction<IContainer> {
 												List<Object> toAdd = new ArrayList<Object>(list);
 												for (Object o : toAdd) {
 													Object targetList = di.eGet(a2);
-													((List<Object>) targetList).add(o);
+													if (attr.equals(ModelPackage.eINSTANCE.getWireDestination_InWires())) {
+														if (o instanceof Wire) {
+															((List<Object>) targetList).add(o);
+														}
+													} else if (attr.equals(ModelPackage.eINSTANCE.getWireSource_OutWires())) {
+														if (o instanceof Wire) {
+															((List<Object>) targetList).add(o);
+														}
+													} else {
+														((List<Object>) targetList).add(o);
+													}
 												}
 											}
 											
@@ -246,7 +277,344 @@ public class MigrateAllModelsAction2 extends ProgressEnabledAction<IContainer> {
 					}
 				}
 				
+				// intermediary save				
+				model.eResource().save( ModelLoader.getSaveOptions() );
+				
+				// merge SelectWire into the DomainIterator
+				
+				{
+					// modify it
+					List<EObject> allObjects = new ArrayList<EObject>();
+					Iterator<EObject> it = model.eAllContents();
+					while (it.hasNext()) {
+						allObjects.add(it.next());
+					}
+					for (EObject obj : allObjects) {
+						EObject container = obj.eContainer();
+						
+						if (obj instanceof SelectWire) {
+							SelectWire wire = (SelectWire) obj;
+							
+							DomainIterator to;
+							if (wire.getTo() instanceof DomainAttributeInstance) {
+								// if its selecting a DomainAttributeInstance, we will
+								// create a DomainIterator explicitly
+								DomainAttributeInstance dai = (DomainAttributeInstance) wire.getTo();
+								if (dai.eContainer() instanceof Scope) {
+									Scope cc = (Scope) dai.eContainer();
+									
+									to = DomainFactory.eINSTANCE.createDomainIterator();
+									to.setName("Iterator for " + dai.getName());
+									to.getAttributes().add(dai);
+									cc.getElements().add(to);
+								} else {
+									throw new RuntimeException("DomainAttributeInstance was not contained within a Scope: " + dai + ", " + dai.eContainer());
+								}
+							} else {
+								if (!(wire.getTo() instanceof DomainIterator)) {
+									throw new RuntimeException("wire.to is not a DomainIterator: " + wire.getTo());
+								}
+								
+								to = (DomainIterator) wire.getTo();
+							}
+							
+							if (!(wire.getFrom() instanceof DomainSchema)) {
+								throw new RuntimeException("wire.from is not a DomainSchema: " + wire.getFrom());
+							}
+							
+							DomainSchema from = (DomainSchema) wire.getFrom();
+							DomainIterator di = to;
+							SelectWire doi = wire;
+							
+							// derive the Source
+							DomainSource source = from.getInSchemas().get(0).getFrom();
+							if (source == null) {
+								throw new RuntimeException("Edge unexpectedly was null: " + from);
+							}
+							
+							// create a SourceEdge to connect the two
+							{
+								SelectEdge edge = DomainFactory.eINSTANCE.createSelectEdge();
+								edge.setTo(source);
+								edge.setFrom(to);
+								source.getSelectEdges().add(edge);
+							}
+							
+							// put this SelectWire information into the iterator
+							to.setQuery(wire.getQuery());
+							to.setLimit(wire.getLimit());
+							to.setOrderAscending(wire.isOrderAscending());
+							to.setOrderBy(wire.getOrderBy());
+							
+							wire.setFrom(null);
+							wire.setTo(null);
+							wire.setOrderBy(null);
+				
+							for (EAttribute attr : doi.eClass().getEAllAttributes()) {
+								if (attr.equals(ModelPackage.eINSTANCE.getGeneratedElement_Id())) {
+									// ignore 'id'
+									continue;
+								}
+								
+								if (attr.equals(WiresPackage.eINSTANCE.getSelectWire_Limit())) {
+									// ignore 'limit'
+									continue;
+								}
+
+								if (attr.equals(WiresPackage.eINSTANCE.getSelectWire_Query())) {
+									// ignore 'query'
+									continue;
+								}
+
+								if (attr.equals(WiresPackage.eINSTANCE.getSelectWire_OrderAscending())) {
+									// ignore 'orderAscending'
+									continue;
+								}
+
+								// find the target EAttribute
+								boolean found = false;
+								for (EAttribute a2 : di.eClass().getEAllAttributes()) {
+									if (a2.equals(attr)) {
+										di.eSet(a2, doi.eGet(attr));
+										found = true;
+										break;
+									}
+								}
+								
+								if (!found) {
+									System.out.println(doi.eClass().getEAllAttributes());
+									System.out.println("--");
+									System.out.println(di.eClass().getEAllAttributes());
+									throw new RuntimeException(di.eClass() + " does not contain " + attr);
+								}
+								
+							}
+							
+							for (EReference attr : doi.eClass().getEAllReferences()) {
+								if (attr.equals(WiresPackage.eINSTANCE.getSelectWire_OrderBy())) {
+									// ignore 'orderBy'
+									continue;
+								}
+								
+								if (attr.equals(ModelPackage.eINSTANCE.getWire_From())) {
+									// ignore 'from'
+									continue;
+								}
+
+								if (attr.equals(ModelPackage.eINSTANCE.getWire_To())) {
+									// ignore 'to'
+									continue;
+								}
+								
+								// ignore empty references
+								if (attr.isMany() && ((List) doi.eGet(attr)).isEmpty())
+									continue;
+								if (!attr.isMany() && doi.eGet(attr) == null)
+									continue;
+								
+								// find the target EReference
+								boolean found = false;
+								for (EReference a2 : di.eClass().getEAllReferences()) {
+									if (a2.equals(attr) || matches(attr, a2)) {
+										if (!attr.isMany()) {
+											di.eSet(a2, doi.eGet(attr));									
+										} else {
+											List<Object> list = (List<Object>) doi.eGet(attr);									
+											List<Object> toAdd = new ArrayList<Object>(list);
+											for (Object o : toAdd) {
+												Object targetList = di.eGet(a2);
+												((List<Object>) targetList).add(o);
+											}
+										}
+										
+										found = true;
+										break;
+									}
+								}
+								
+								if (!found) {
+									System.out.println(doi.eClass().getEAllReferences());
+									System.out.println("--");
+									System.out.println(di.eClass().getEAllReferences());
+									throw new RuntimeException(di.eClass().getName() + " does not contain " + attr.getName() + " from " + doi.eClass().getName());
+								}
+								
+							}
+	
+							// replace the new DomainIterator
+							// and remove the current DOI
+							
+							
+							if (wire.eContainer() instanceof ContainsWires) {
+								ContainsWires cw = (ContainsWires) wire.eContainer();
+								cw.getWires().remove(wire);
+							} else {
+								throw new RuntimeException("Wire container was not a ContainsWires: " + wire.eContainer());
+							}
 					
+							
+						}
+						
+					}
+				}
+				
+				// merge NewInstanceWire into the DomainIterator
+				
+				{
+					// modify it
+					List<EObject> allObjects = new ArrayList<EObject>();
+					Iterator<EObject> it = model.eAllContents();
+					while (it.hasNext()) {
+						allObjects.add(it.next());
+					}
+					for (EObject obj : allObjects) {
+						EObject container = obj.eContainer();
+						
+						if (obj instanceof NewInstanceWire) {
+							NewInstanceWire wire = (NewInstanceWire) obj;
+							
+							DomainIterator to;
+							if (wire.getTo() instanceof DomainAttributeInstance) {
+								// if its selecting a DomainAttributeInstance, we will
+								// create a DomainIterator explicitly
+								DomainAttributeInstance dai = (DomainAttributeInstance) wire.getTo();
+								if (dai.eContainer() instanceof Scope) {
+									Scope cc = (Scope) dai.eContainer();
+									
+									to = DomainFactory.eINSTANCE.createDomainIterator();
+									to.setName("Iterator for " + dai.getName());
+									to.getAttributes().add(dai);
+									cc.getElements().add(to);
+								} else {
+									throw new RuntimeException("DomainAttributeInstance was not contained within a Scope: " + dai + ", " + dai.eContainer());
+								}
+							} else {
+								if (!(wire.getTo() instanceof DomainIterator)) {
+									throw new RuntimeException("wire.to is not a DomainIterator: " + wire.getTo());
+								}
+								
+								to = (DomainIterator) wire.getTo();
+							}
+							
+							if (!(wire.getFrom() instanceof DomainSchema)) {
+								throw new RuntimeException("wire.from is not a DomainSchema: " + wire.getFrom());
+							}
+							
+							DomainSchema from = (DomainSchema) wire.getFrom();
+							DomainIterator di = to;
+							NewInstanceWire doi = wire;
+							
+							// derive the Source
+							DomainSource source = from.getInSchemas().get(0).getFrom();
+							if (source == null) {
+								throw new RuntimeException("Edge unexpectedly was null: " + from);
+							}
+							
+							// create a SourceEdge to connect the two
+							{
+								SelectEdge edge = DomainFactory.eINSTANCE.createSelectEdge();
+								edge.setTo(source);
+								edge.setFrom(to);
+								source.getSelectEdges().add(edge);
+							}
+							
+							// put this SelectWire information into the iterator
+							to.setQuery("new");
+							
+							wire.setFrom(null);
+							wire.setTo(null);
+				
+							for (EAttribute attr : doi.eClass().getEAllAttributes()) {
+								if (attr.equals(ModelPackage.eINSTANCE.getGeneratedElement_Id())) {
+									// ignore 'id'
+									continue;
+								}
+								
+								// find the target EAttribute
+								boolean found = false;
+								for (EAttribute a2 : di.eClass().getEAllAttributes()) {
+									if (a2.equals(attr)) {
+										di.eSet(a2, doi.eGet(attr));
+										found = true;
+										break;
+									}
+								}
+								
+								if (!found) {
+									System.out.println(doi.eClass().getEAllAttributes());
+									System.out.println("--");
+									System.out.println(di.eClass().getEAllAttributes());
+									throw new RuntimeException(di.eClass() + " does not contain " + attr);
+								}
+								
+							}
+							
+							for (EReference attr : doi.eClass().getEAllReferences()) {
+								if (attr.equals(ModelPackage.eINSTANCE.getWire_From())) {
+									// ignore 'from'
+									continue;
+								}
+
+								if (attr.equals(ModelPackage.eINSTANCE.getWire_To())) {
+									// ignore 'to'
+									continue;
+								}
+								
+								// ignore empty references
+								if (attr.isMany() && ((List) doi.eGet(attr)).isEmpty())
+									continue;
+								if (!attr.isMany() && doi.eGet(attr) == null)
+									continue;
+								
+								// find the target EReference
+								boolean found = false;
+								for (EReference a2 : di.eClass().getEAllReferences()) {
+									if (a2.equals(attr) || matches(attr, a2)) {
+										if (!attr.isMany()) {
+											di.eSet(a2, doi.eGet(attr));									
+										} else {
+											List<Object> list = (List<Object>) doi.eGet(attr);									
+											List<Object> toAdd = new ArrayList<Object>(list);
+											for (Object o : toAdd) {
+												Object targetList = di.eGet(a2);
+												((List<Object>) targetList).add(o);
+											}
+										}
+										
+										found = true;
+										break;
+									}
+								}
+								
+								if (!found) {
+									System.out.println(doi.eClass().getEAllReferences());
+									System.out.println("--");
+									System.out.println(di.eClass().getEAllReferences());
+									throw new RuntimeException(di.eClass().getName() + " does not contain " + attr.getName() + " from " + doi.eClass().getName());
+								}
+								
+							}
+	
+							// replace the new DomainIterator
+							// and remove the current DOI
+							
+							
+							if (wire.eContainer() instanceof ContainsWires) {
+								ContainsWires cw = (ContainsWires) wire.eContainer();
+								cw.getWires().remove(wire);
+							} else {
+								throw new RuntimeException("Wire container was not a ContainsWires: " + wire.eContainer());
+							}
+					
+							
+						}
+						
+					}
+				}
+				
+				// remove all DomainStores
+				model.getDomainStores().clear();
+				
 				// finally save it				
 				model.eResource().save( ModelLoader.getSaveOptions() );
 				

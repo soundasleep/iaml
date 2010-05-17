205d902b56d19a18c8ac9fd56a7b7ebfd439b3aa
Jevon Wright <soundasleep@users.noreply.github.com>
2010-05-17 08:12:25 +0000

    adding a delay to instant SyncWires, to reduce network traffic

--- org.openiaml.model.tests/src/org/openiaml/model/tests/codegen/model0_5_2/SyncWireInstant.java
+++ org.openiaml.model.tests/src/org/openiaml/model/tests/codegen/model0_5_2/SyncWireInstant.java
@@ -3,21 +3,12 @@
  */
 package org.openiaml.model.tests.codegen.model0_5_2;
 
-import java.io.IOException;
-import java.util.List;
-
-import net.sourceforge.jwebunit.api.IElement;
-import net.sourceforge.jwebunit.htmlunit.HtmlUnitElementImpl;
-
-import org.openiaml.model.tests.CodegenTestCase;
-
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
 
 /**
  * Tests SyncWires with executeOnInput = true. Also tests 
  * InputTextField.onInput.
  */
-public class SyncWireInstant extends CodegenTestCase {
+public class SyncWireInstant extends TypedCodegenTestCase {
 	
 	@Override
 	protected void setUp() throws Exception {
@@ -202,67 +193,6 @@ public class SyncWireInstant extends CodegenTestCase {
 		}
 
 		assertNoProblem();
-	}
-
-	/**
-	 * Type in the given text to the Input element labelled by the given element ID.
-	 * 
-	 * @throws IOException 
-	 */
-	public void typeLabeledFormElement(String id, String text) throws IOException {
-		// get the elements referenced by this label
-		IElement label = getElementById(id);
-		assertNotNull(label);
-		
-		List<IElement> elements = getFieldsForLabel(label);
-		assertFalse("Could not find any elements for label " + id, elements.isEmpty());
-		
-		for (IElement element : elements) {
-			// type it into this label
-			assertTrue("Expected HtmlUnitElementImpl, but was " + element.getClass().getName(),
-					element instanceof HtmlUnitElementImpl);
-
-			// get the actual HtmlUnit element
-			HtmlElement actual = ((HtmlUnitElementImpl) element).getHtmlElement();
-			assertNotNull(actual);
-			
-			// type in the text
-			actual.type(text);
-			
-			// and return
-			return;
-		}	
-	}
-	
-	/**
-	 * blur() on the Input element labelled by the given element ID.
-	 * 
-	 * @param id
-	 */
-	public void loseFocus(String id) {
-		// get the elements referenced by this label
-		IElement label = getElementById(id);
-		assertNotNull(label);
-		
-		List<IElement> elements = getFieldsForLabel(label);
-		assertFalse("Could not find any elements for label " + id, elements.isEmpty());
-		
-		for (IElement element : elements) {
-			// type it into this label
-			assertTrue("Expected HtmlUnitElementImpl, but was " + element.getClass().getName(),
-					element instanceof HtmlUnitElementImpl);
-
-			// get the actual HtmlUnit element
-			HtmlElement actual = ((HtmlUnitElementImpl) element).getHtmlElement();
-			assertNotNull(actual);
-			
-			// lose focus
-			actual.blur();
-			
-			// and return
-			return;
-		}	
-		
 	}
 
 

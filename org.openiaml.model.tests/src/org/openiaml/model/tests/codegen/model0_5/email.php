51987229c13e65fb81dc7bbf9d75b29b6f2671cc
Jevon Wright <soundasleep@users.noreply.github.com>
2010-07-12 03:20:05 +0000

    creating a branch for testing the performance of drools rules

--- org.openiaml.model.tests/src/org/openiaml/model/tests/codegen/model0_5/email.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This is an overridden template.
-?>
-This e-mail, from <?php echo $property["from"]; ?> to <?php echo $property["to"]; ?>, has these field values: <?php echo $label["field 1"]; ?>, <?php echo $label["field 2"]; ?>, <?php echo $label["field 3"]; ?>.

111c82e34995837e8f4d88d3b61d50dfc690e144
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2011-01-12 05:36:16 +0000

    writing a new modeldoc function, convertHTMLIntoLatex(), which tries make a javadoc-compatible string with HTML tags (e.g. <code>) into LaTeX-suitable code (e.g. \code{})

--- trunk/org.openiaml.docs/src/org/openiaml/docs/tests/TestLatexFunctions.java
+++ trunk/org.openiaml.docs/src/org/openiaml/docs/tests/TestLatexFunctions.java
@@ -8,15 +8,33 @@ import junit.framework.TestCase;
 import org.openiaml.docs.generation.codegen.LatexCodegenFunctions;
 
 /**
+ * Tests the functionality in {@link LatexCodegenFunctions}.
+ * 
  * @author jmwright
  *
  */
 public class TestLatexFunctions extends TestCase {
 
+	/**
+	 * Tests {@link LatexCodegenFunctions#humanise(String)}.
+	 * 
+	 * @throws Exception
+	 */
 	public void testHumanise() throws Exception {
-		
 		assertEquals("Input Text Field", LatexCodegenFunctions.humanise("InputTextField"));
-		
+	}
+	
+	/**
+	 * Tests {@link LatexCodegenFunctions#convertHTMLIntoLatex(String)}.
+	 * 
+	 * @throws Exception
+	 */
+	public void testConvertHTMLIntoLatex() throws Exception {
+		assertEquals("test", LatexCodegenFunctions.convertHTMLIntoLatex("test"));
+		assertEquals("test\\_test", LatexCodegenFunctions.convertHTMLIntoLatex("test_test"));
+		assertEquals("test \\& test", LatexCodegenFunctions.convertHTMLIntoLatex("test &amp; test"));
+		assertEquals("<hello>", LatexCodegenFunctions.convertHTMLIntoLatex("&lt;hello&gt;"));
+		assertEquals("hello \\code{world} world", LatexCodegenFunctions.convertHTMLIntoLatex("hello <code>world</code> world"));
 	}
 	
 }

880dc1a1224d806be388f9195e7db7e4a097b965
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2011-04-01 03:40:42 +0000

    total file metrics can now be accumulated adding tests and tests.diagram to plugins to be metricised

--- trunk/org.openiaml.docs/src/org/openiaml/docs/generation/LoadFileMetrics.java
+++ trunk/org.openiaml.docs/src/org/openiaml/docs/generation/LoadFileMetrics.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.openiaml.docs.modeldoc.Metric;
 import org.openiaml.docs.modeldoc.ModelDocumentation;
 import org.openiaml.docs.modeldoc.ModeldocFactory;
@@ -19,6 +20,74 @@ import org.openiaml.docs.modeldoc.ModeldocFactory;
  */
 public class LoadFileMetrics extends DocumentationHelper implements ILoader {
 
+	/**
+	 * This is a helper class that accumulates the results of a number of
+	 * {@link LoadFileMetrics} helpers into a single set of accumulated metrics.
+	 * For example, this can be used to identify the <em>total</em> number of
+	 * HTML files across <em>all</em> loaded plugins. 
+	 * 
+	 * @author jmwright
+	 *
+	 */
+	public static class FileMetricAccumulator {
+		
+		/**
+		 * The loaded map of total file counts.
+		 */
+		private Map<String,Long> totalCountMap = new HashMap<String,Long>();
+		
+		/**
+		 * The loaded map of total file sizes.
+		 */
+		private Map<String,Long> totalSizeMap = new HashMap<String,Long>();
+		
+		
+		/**
+		 * Load all of the file metrics loaded via the given helper into the
+		 * accumulator.
+		 * 
+		 * @param helper
+		 */
+		public void add(LoadFileMetrics helper) {
+			if (helper.fileCountMap == null || helper.fileSizeMap == null) {
+				throw new RuntimeException("LoadFileMetrics has not yet been evaluated on a plugin.");
+			}
+			
+			for (String extension : helper.fileCountMap.keySet()) {
+				increment(totalCountMap, extension, helper.fileCountMap.get(extension));
+			}
+			
+			for (String extension : helper.fileSizeMap.keySet()) {
+				increment(totalSizeMap, extension, helper.fileCountMap.get(extension));
+			}
+		}
+		
+		/**
+		 * Publish the accumulated metrics into the given modeldoc instance.
+		 */
+		public void publish(ModeldocFactory factory, ModelDocumentation root) {
+			// now save all of the metrics back
+			// file count
+			for (String extension : totalCountMap.keySet()) {
+				Metric metric = factory.createMetric();
+				// e.g. file.java.count.org.openiaml.model
+				metric.setName("fileCount." + extension + ".total");
+				metric.setValue(totalCountMap.get(extension).toString());
+				root.getMetrics().add(metric);
+			}
+			
+			// file size
+			for (String extension : totalSizeMap.keySet()) {
+				Metric metric = factory.createMetric();
+				// e.g. file.java.size.org.openiaml.model
+				metric.setName("fileSize." + extension + ".total");
+				metric.setValue(totalSizeMap.get(extension).toString());
+				root.getMetrics().add(metric);
+			}
+		}
+		
+	}
+	
 	/**
 	 * The reference to the plugin directory, e.g. "../org.openiaml.model".
 	 */
@@ -35,6 +104,16 @@ public class LoadFileMetrics extends DocumentationHelper implements ILoader {
 		this.root = root;
 		this.plugin = plugin;
 	}
+	
+	/**
+	 * The loaded map of file counts.
+	 */
+	private Map<String,Long> fileCountMap;
+	
+	/**
+	 * The loaded map of file sizes.
+	 */
+	private Map<String,Long> fileSizeMap;
 
 	/**
 	 * Iterate over the given plugin and find out file statistics of all
@@ -46,9 +125,9 @@ public class LoadFileMetrics extends DocumentationHelper implements ILoader {
 	public void load(ModeldocFactory factory, ModelDocumentation root) {
 
 		// map of (file extensions) to (count)
-		Map<String,Long> fileCountMap = new HashMap<String,Long>();
+		fileCountMap = new HashMap<String,Long>();
 		// map of (file extensions) to (size)
-		Map<String,Long> fileSizeMap = new HashMap<String,Long>();
+		fileSizeMap = new HashMap<String,Long>();
 		
 		// start recurse
 		load(this.root, fileCountMap, fileSizeMap);
@@ -124,7 +203,7 @@ public class LoadFileMetrics extends DocumentationHelper implements ILoader {
 	 * Increment the given map at the given key value with the given value.
 	 * If the key does not exist, creates it.
 	 */
-	private void increment(Map<String,Long> map, String key, long value) {
+	private static void increment(Map<String,Long> map, String key, long value) {
 		if (map.containsKey(key)) {
 			map.put(key, map.get(key) + value);
 		} else {
@@ -135,7 +214,7 @@ public class LoadFileMetrics extends DocumentationHelper implements ILoader {
 	 * Increment the given map at the given key value by 1.
 	 * If the key does not exist, creates it.
 	 */
-	private void increment(Map<String,Long> map, String key) {
+	private static void increment(Map<String,Long> map, String key) {
 		increment(map, key, 1);
 	}
 	

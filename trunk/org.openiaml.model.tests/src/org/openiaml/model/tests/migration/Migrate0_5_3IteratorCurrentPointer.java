c67d28c2fef61d3eb3c25257a25d20cbe0a816ce
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2011-04-28 05:34:30 +0000

    adding test case for issue 241 model migration

---
+++ trunk/org.openiaml.model.tests/src/org/openiaml/model/tests/migration/Migrate0_5_3IteratorCurrentPointer.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package org.openiaml.model.tests.migration;
+
+import java.util.List;
+
+import org.openiaml.model.migrate.IamlModelMigrator;
+import org.openiaml.model.migrate.Migrate0To1;
+import org.openiaml.model.migrate.Migrate1To2;
+import org.openiaml.model.migrate.Migrate2To4;
+import org.openiaml.model.migrate.Migrate4To5;
+import org.openiaml.model.migrate.Migrate5To6;
+import org.openiaml.model.model.InternetApplication;
+
+/**
+ * Tests that DomainIterators and their contained DomainAttributeInstances are migrated correctly.
+ * 
+ * @see #getModel()
+ * @author jmwright
+ *
+ */
+public class Migrate0_5_3IteratorCurrentPointer extends AbstractMigrateTestCase {
+
+	/**
+	 * Test to see which migrators were actually used.
+	 * 
+	 * @throws Exception
+	 */
+	public void testMigratorsUsed() throws Exception {
+		List<IamlModelMigrator> used = migrateModelOnly();
+		assertClassNotIn(Migrate0To1.class, used);
+		assertClassNotIn(Migrate1To2.class, used);
+		assertClassNotIn(Migrate2To4.class, used);
+		assertClassNotIn(Migrate4To5.class, used);
+		assertClassIn(Migrate5To6.class, used);
+	}
+		
+	@Override
+	public String getModel() {
+		return "IteratorCurrentPointer-0_5_3.iaml";
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openiaml.model.tests.migration.AbstractMigrateTestCase#migratedModelTests(org.openiaml.model.model.InternetApplication)
+	 */
+	@Override
+	public void migratedModelTests(InternetApplication root) throws Exception {
+		// TODO currently empty
+	}
+	
+}

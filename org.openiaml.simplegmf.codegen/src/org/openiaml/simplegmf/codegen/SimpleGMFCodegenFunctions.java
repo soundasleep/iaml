0761fedb55e0a086c7287f60f9595fd0a1dff750
Jevon Wright <soundasleep@users.noreply.github.com>
2011-05-01 03:43:25 +0000

    simplegmf: adding the structure for code generation, based on modeldoc codegen (from oaw)

---
+++ org.openiaml.simplegmf.codegen/src/org/openiaml/simplegmf/codegen/SimpleGMFCodegenFunctions.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.openiaml.simplegmf.codegen;
+
+
+/**
+ * Runtime functions for SimpleGMF codegen.
+ * 
+ * @author jmwright
+ *
+ */
+public class SimpleGMFCodegenFunctions {
+	
+}

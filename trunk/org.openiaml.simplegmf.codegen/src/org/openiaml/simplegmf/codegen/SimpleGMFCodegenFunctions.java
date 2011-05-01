2c8de99cc619fcfaa1a2e34f1cd1116ece8f53e6
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2011-05-01 08:20:03 +0000

    simplegmf: initial work in generating XXX.gmfgraph adding local copy of Ecore metamodel

--- trunk/org.openiaml.simplegmf.codegen/src/org/openiaml/simplegmf/codegen/SimpleGMFCodegenFunctions.java
+++ trunk/org.openiaml.simplegmf.codegen/src/org/openiaml/simplegmf/codegen/SimpleGMFCodegenFunctions.java
@@ -3,8 +3,12 @@
  */
 package org.openiaml.simplegmf.codegen;
 
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.openiaml.simplegmf.model.simplegmf.BuiltinShapes;
+import org.openiaml.simplegmf.model.simplegmf.FigureConfiguration;
+import org.openiaml.simplegmf.model.simplegmf.GMFConfiguration;
 
 
 /**
@@ -20,6 +24,40 @@ public class SimpleGMFCodegenFunctions {
 			EcoreUtil.resolve(o, CurrentModel.getCurrentModel().eResource() );
 		
 		return o == null ? "null" : o.toString();
+	}
+	
+	public static String toStringOrNull(String s) {
+		return s == null ? "" : s;
+	}
+	
+	/**
+	 * Get the first shape classifier that is not default, that
+	 * matches the given classifier.
+	 * 
+	 * @param root
+	 * @param classifier
+	 * @return
+	 */
+	public static String getShapeType(GMFConfiguration root, EClass classifier) {
+		// do any figure configurations exist for this classifier?
+		for (FigureConfiguration fc : root.getFigureConfigurations()) {
+			if (fc.getClassifier() != null 
+					&& fc.getClassifier().isSuperTypeOf(classifier)
+					&& (
+						/* a default shape */ !fc.getShape().toString().equals("DEFAULT"))) {
+				return fc.getShape().toString();
+			}
+		}
+		
+		// get the default shape
+		for (FigureConfiguration fc : root.getFigureConfigurations()) {
+			if (fc.isIsDefault()) {
+				return fc.getShape().toString();
+			}
+		}
+		
+		throw new RuntimeException("Found no default FigureConfigurations for " + classifier);
+		
 	}
 	
 }

5a86a14129dd70c0ba3534472478776417eefb5f
Jevon Wright <soundasleep@users.noreply.github.com>
2011-05-03 07:31:15 +0000

    issue 242: changing all type references to EXSDDataType creating a new helper class InitialModel to populate the initial model

---
+++ org.openiaml.model/src/org/openiaml/model/InitialModel.java
@@ -0,0 +1,145 @@
+/**
+ * 
+ */
+package org.openiaml.model;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xsd.XSDSchema;
+import org.eclipse.xsd.XSDSimpleTypeDefinition;
+import org.eclipse.xsd.XSDTypeDefinition;
+import org.openiaml.model.ModelLoader.ModelLoadException;
+import org.openiaml.model.model.EXSDDataType;
+import org.openiaml.model.model.InternetApplication;
+import org.openiaml.model.model.ModelFactory;
+import org.openiaml.model.model.visual.Frame;
+import org.openiaml.model.model.visual.VisualFactory;
+
+/**
+ * Static method to create a new model instance. Essentially takes the code
+ * out of dynamic templates (<code>templates/aspects/xpt/editor/DiagramEditorUtil.xpt</code>)
+ * and into a Java class.
+ * 
+ * @author jmwright
+ *
+ */
+public class InitialModel {
+	
+	/**
+	 * Represents an exception that occured while trying to initialise a new model
+	 * instance.
+	 * 
+	 * @author jmwright
+	 *
+	 */
+	public static class ModelInitializationException extends Exception {
+
+		private static final long serialVersionUID = 1L;
+
+		public ModelInitializationException(String message) {
+			super(message);
+		}
+
+		public ModelInitializationException(String message, ModelLoadException e) {
+			super(message, e);
+		}
+		
+	}
+	
+	private static XSDSchema xsdSchemaInstance = null;
+	
+	private static void initialiseEMFXSDDataTypes() throws ModelInitializationException {
+		if (xsdSchemaInstance == null) {
+			URI uri = URI.createPlatformPluginURI("/org.eclipse.xsd/cache/www.w3.org/2001/XMLSchema.xsd", false);
+			EObject model;
+			try {
+				model = ModelLoader.load(uri);
+			} catch (ModelLoadException e) {
+				throw new ModelInitializationException("Could not load " + uri + ": " + e.getMessage(), e);
+			}
+			if (!(model instanceof XSDSchema)) {
+				throw new ModelInitializationException("Could not load " + uri + ": loaded model was not an XSDSchema");
+			}
+			xsdSchemaInstance = (XSDSchema) model;
+		}
+	}
+	
+	private static void addEMFXSDDataTypes(InternetApplication app) throws ModelInitializationException {
+		initialiseEMFXSDDataTypes();
+		
+		for (XSDTypeDefinition type : xsdSchemaInstance.getTypeDefinitions()) {
+			if (type instanceof XSDSimpleTypeDefinition) {
+				XSDSimpleTypeDefinition xs = (XSDSimpleTypeDefinition) type;
+				
+				EXSDDataType dt = ModelFactory.eINSTANCE.createEXSDDataType();
+				dt.setId("xsd:" + xs.getName());
+				dt.setDefinition(xs);
+				dt.setName("xsd:" + xs.getName());
+				app.getXsdDataTypes().add(dt);
+			}
+		}
+	}
+
+	private static XSDSchema builtinXsdTypes = null;
+	
+	private static void initialiseBuiltinXSDDataTypes() throws ModelInitializationException {
+		if (builtinXsdTypes == null) {
+			URI uri = URI.createPlatformPluginURI("/org.openiaml.model/model/datatypes.xsd", false);
+			EObject model;
+			try {
+				model = ModelLoader.load(uri);
+			} catch (ModelLoadException e) {
+				throw new ModelInitializationException("Could not load " + uri + ": " + e.getMessage(), e);
+			}
+			if (!(model instanceof XSDSchema)) {
+				throw new ModelInitializationException("Could not load " + uri + ": loaded model was not an XSDSchema");
+			}
+			builtinXsdTypes = (XSDSchema) model;
+		}
+	}
+	
+	private static void addBuiltinXSDDataTypes(InternetApplication app) throws ModelInitializationException {
+		initialiseBuiltinXSDDataTypes();
+		
+		for (XSDTypeDefinition type : builtinXsdTypes.getTypeDefinitions()) {
+			if (type instanceof XSDSimpleTypeDefinition) {
+				XSDSimpleTypeDefinition xs = (XSDSimpleTypeDefinition) type;
+				
+				EXSDDataType dt = ModelFactory.eINSTANCE.createEXSDDataType();
+				dt.setId("builtin:" + xs.getName());
+				dt.setDefinition(xs);
+				dt.setName("builtin:" + xs.getName());
+				app.getXsdDataTypes().add(dt);
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param app the initially created root model element; this element is
+	 * 		empty.
+	 */
+	public static void createInitialModel(InternetApplication app) {
+		
+		try {
+		
+			Frame frame = VisualFactory.eINSTANCE.createFrame();
+			frame.setName("Home");
+			frame.setId("index");	// default page
+			app.getScopes().add(frame);
+			
+			// load EMF XSD data types
+			addEMFXSDDataTypes(app);
+			
+			// load custom XSD data types
+			addBuiltinXSDDataTypes(app);
+		
+		} catch (ModelInitializationException e) {
+			IamlModelPlugin.getInstance().logError("Could not initialise model: " + e.getMessage(), e);
+		}
+		
+	}
+	
+}

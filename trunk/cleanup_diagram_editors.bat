c81abb1eadb70cdc632b30dc52e4dd8bfd97da40
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2011-05-07 01:16:39 +0000

    adding a batch script as per the wiki page GeneratingDiagramEditors

---
+++ trunk/cleanup_diagram_editors.bat
@@ -0,0 +1,13 @@
+rem we should be in workspace\
+rm -rf "org.openiaml.model.diagram\src\org"
+rm -rf "org.openiaml.model.diagram.condition\src\org"
+rm -rf "org.openiaml.model.diagram.element\src\org"
+rm -rf "org.openiaml.model.diagram.email\src\org"
+rm -rf "org.openiaml.model.diagram.frame\src\org"
+rm -rf "org.openiaml.model.diagram.instance\src\org"
+rm -rf "org.openiaml.model.diagram.iterator\src\org"
+rm -rf "org.openiaml.model.diagram.operation\src\org"
+rm -rf "org.openiaml.model.diagram.root\src\org"
+rm -rf "org.openiaml.model.diagram.schema\src\org"
+rm -rf "org.openiaml.model.diagram.session\src\org"
+rm -rf "org.openiaml.model.diagram.visual\src\org"

78b5a68c29e5e6f9e6f21a92667f87c704e7d292
Jevon Wright <soundasleep@users.noreply.github.com>
2011-05-09 06:15:42 +0000

    adding a hook to AllTests to automatically capture a copy of all inferred models

--- org.openiaml.model.tests/src/org/openiaml/model/tests/AllTests.java
+++ org.openiaml.model.tests/src/org/openiaml/model/tests/AllTests.java
@@ -1,6 +1,12 @@
 package org.openiaml.model.tests;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import org.openiaml.model.tests.codegen.AllCodegenTests;
@@ -29,6 +35,9 @@ public class AllTests {
     public static Test suite() { 
         TestSuite suite = new TestSuite("All Tests");
 
+        // delete all infer-output models
+        suite.addTestSuite(ClearInferredModels.class);
+
         suite.addTest(AllInferenceTests.suite());
         suite.addTest(AllCodegenTests.suite());
         suite.addTest(AllDroolsTests.suite());
@@ -36,7 +45,67 @@ public class AllTests {
         suite.addTest(AllMigrationTests.suite());
         suite.addTest(AllModelTests.suite());
 
-        return suite; 
-   }
+        suite.addTestSuite(CommitInferredModels.class);
 
+        return suite; 
+    }
+    
+    /**
+     * A helper "test case" that simply deletes all of the folders in infer-output.
+     * 
+     */
+    public static class ClearInferredModels extends TestCase {
+    	public ClearInferredModels() {
+    		
+    	}
+    	
+    	public void testClearInferredModels() throws Exception {
+    		File infer = new File("infer-output");
+    		assertTrue(infer.exists());
+    		assertTrue(infer.isDirectory());
+    		
+    		// check that the batch script exists
+    		assertTrue(new File("infer-output/clear_models.bat").exists());
+    		
+    		Runtime.getRuntime().exec(
+    				"cmd /c start clear_models.bat",
+    				getEnvironmentVariables(),
+    				infer);
+    	}
+
+    }
+    
+    /**
+     * A helper "test case" that simply commits all of the files in infer-output.
+     * 
+     */
+	public static class CommitInferredModels extends TestCase {
+		public CommitInferredModels() {
+			
+		}
+		
+		public void testCommitInferredModels() throws Exception {
+    		File infer = new File("infer-output");
+    		assertTrue(infer.exists());
+    		assertTrue(infer.isDirectory());
+    		
+    		// check that the batch script exists
+    		assertTrue(new File("infer-output/commit_models.bat").exists());
+    		
+    		Runtime.getRuntime().exec(
+    				"cmd /c start commit_models.bat", 
+    				getEnvironmentVariables(),
+    				infer);
+    	}
+	}
+
+	private static String[] getEnvironmentVariables() {
+		Map<String, String> env = System.getenv();
+		List<String> buf = new ArrayList<String>();
+		for (String key : env.keySet()) {
+			buf.add(key + "=" + env.get(key));
+		}
+		return buf.toArray(new String[] {});
+	}
+	
 }

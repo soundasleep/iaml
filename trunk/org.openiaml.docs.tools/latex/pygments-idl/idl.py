e675ad72f2ee108b4f0b735f9926e26d07d79194
soundasleep <soundasleep@df845254-314f-0410-8fd9-59f6c26b83b8>
2011-07-08 05:38:03 +0000

    creating a Pygments style for IDL interface definitions

---
+++ trunk/org.openiaml.docs.tools/latex/pygments-idl/idl.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+    pygments.lexers.idl
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Lexer for IDL.
+    Based on pygments.lexers.compiled
+
+    :license: BSD, see LICENSE for details.
+"""
+
+import re
+
+from pygments.scanner import Scanner
+from pygments.lexer import Lexer, RegexLexer, include, bygroups, using, \
+                           this, combined
+from pygments.util import get_bool_opt, get_list_opt
+from pygments.token import \
+     Text, Comment, Operator, Keyword, Name, String, Number, Punctuation, \
+     Error
+
+# backwards compatibility
+from pygments.lexers.functional import OcamlLexer
+
+__all__ = ['IDLLexer']
+
+# based on JavaLexer
+class IDLLexer(RegexLexer):
+    """
+    For IDL source code.
+    """
+
+    name = 'IDL'
+    aliases = ['idl']
+    filenames = ['*.idl']
+    mimetypes = ['text/x-idl']
+
+    flags = re.MULTILINE | re.DOTALL
+
+    #: optional Comment or Whitespace
+    _ws = r'(?:\s|//.*?\n|/[*].*?[*]/)+'
+
+    tokens = {
+        'root': [
+            # method names
+            (r'^(\s*(?:[a-zA-Z_][a-zA-Z0-9_\.\[\]]*\s+)+?)' # return arguments
+             r'([a-zA-Z_][a-zA-Z0-9_]*)'                    # method name
+             r'(\s*)(\()',                                  # signature start
+             bygroups(using(this), Name.Function, Text, Operator)),
+            (r'[^\S\n]+', Text),
+            (r'//.*?\n', Comment.Single),
+            (r'/\*.*?\*/', Comment.Multiline),
+            (r'@[a-zA-Z_][a-zA-Z0-9_\.]*', Name.Decorator),
+            (r'(readonly|attribute|in|out)\b', Keyword.Declaration),
+            (r'(boolean|byte|char|double|float|int|long|short|void|unsigned)\b',
+             Keyword.Type),
+            (r'(package)(\s+)', bygroups(Keyword.Namespace, Text)),
+            (r'(true|false|null)\b', Keyword.Constant),
+            (r'(class|interface)(\s+)', bygroups(Keyword.Declaration, Text), 'class'),
+            (r'(import)(\s+)', bygroups(Keyword.Namespace, Text), 'import'),
+            (r'"(\\\\|\\"|[^"])*"', String),
+            (r"'\\.'|'[^\\]'|'\\u[0-9a-f]{4}'", String.Char),
+            (r'(\.)([a-zA-Z_][a-zA-Z0-9_]*)', bygroups(Operator, Name.Attribute)),
+            (r'[a-zA-Z_][a-zA-Z0-9_]*:', Name.Label),
+            (r'[a-zA-Z_\$][a-zA-Z0-9_]*', Name),
+            (r'[~\^\*!%&\[\]\(\)\{\}<>\|+=:;,./?-]', Operator),
+            (r'[0-9][0-9]*\.[0-9]+([eE][0-9]+)?[fd]?', Number.Float),
+            (r'0x[0-9a-f]+', Number.Hex),
+            (r'[0-9]+L?', Number.Integer),
+            (r'\n', Text)
+        ],
+        'class': [
+            (r'[a-zA-Z_][a-zA-Z0-9_]*', Name.Class, '#pop')
+        ],
+        'import': [
+            (r'[a-zA-Z0-9_.]+\*?', Name.Namespace, '#pop')
+        ],
+    }
+
